10fa242c18f78003e8adb67dd0c3977b0c744d16
i80287 <addmlbx@gmail.com>
2024-01-01 01:40:42 +0300

    add Sparse table

---
+++ range_queries/sparse_table/main.cpp
@@ -0,0 +1,28 @@
+#include <cassert>
+#include <random>
+#include "SparseTable.cpp"
+
+int main() {
+    using T = int64_t;
+    constexpr T arr[] = {
+        1, -2, -34, -2, 5, -2, 44, 53, 2, 2, 1, 4, 3, 6, 7, 4, 2, 5, 2, 3, 5, 6, 3, 4, 3, 4, 23, 3, 4, -2, -1, -1, 23, -3, 0, 0, 1, 3, 21, -1, 2
+    };
+    const size_t n = sizeof(arr) / sizeof(arr[0]);
+
+    SparseTable<T> sparsetable(arr, n);
+    std::mt19937 rnd(std::random_device{}());
+    for (size_t k = 1 << 25; k != 0; k--) {
+        size_t l = rnd() % n;
+        size_t r = rnd() % n;
+        if (l > r) {
+            std::swap(l, r);
+        }
+
+        T res = arr[l];
+        for (size_t i = l + 1; i <= r; i++) {
+            res = std::min(res, arr[i]);
+        }
+
+        assert(sparsetable(l, r) == res);
+    }
+}
