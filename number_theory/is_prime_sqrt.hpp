0a6be5ce861d518bd811dd42e5f4afd35473381f
i80287 <addmlbx@gmail.com>
2024-01-13 05:09:21 +0300

    update IsPrimeSqrt

--- number_theory/is_prime_sqrt.hpp
+++ number_theory/is_prime_sqrt.hpp
@@ -1,13 +1,14 @@
-#ifndef IS_PRIME_SLOW_H
-#define IS_PRIME_SLOW_H 1
+#ifndef IS_PRIME_SQRT_HPP
+#define IS_PRIME_SQRT_HPP 1
 
 #include <cmath>
 #include <cstdint>
 
+#include "config_macros.hpp"
 #include "math_utils.hpp"
 
 static constexpr bool IsPrimeSqrt(uint64_t n) noexcept {
-	if (n % 2 == 0) {
+    if (n % 2 == 0) {
         return n == 2;
     }
     if (n % 3 == 0) {
@@ -19,18 +20,17 @@ static constexpr bool IsPrimeSqrt(uint64_t n) noexcept {
     if (unlikely(n < 7 * 7)) {
         return n != 1;
     }
-	uint64_t i = 7;
-	const uint64_t root = math_utils::isqrt(n);
-	do {
-		if (n % i == 0 || n % (i + 4) == 0 ||
-			n % (i + 6) == 0 || n % (i + 10) == 0 ||
-			n % (i + 12) == 0 || n % (i + 16) == 0 ||
-			n % (i + 22) == 0 || n % (i + 24) == 0) {
-			return false;
-		}
-		i += 30;
-	} while (i <= root);
-	return true;
+    uint64_t i = 7;
+    const uint64_t root = math_utils::isqrt(n);
+    do {
+        if (n % i == 0 || n % (i + 4) == 0 || n % (i + 6) == 0 ||
+            n % (i + 10) == 0 || n % (i + 12) == 0 || n % (i + 16) == 0 ||
+            n % (i + 22) == 0 || n % (i + 24) == 0) {
+            return false;
+        }
+        i += 30;
+    } while (i <= root);
+    return true;
 }
 
-#endif // !IS_PRIME_SLOW_H
+#endif  // !IS_PRIME_SQRT_HPP
