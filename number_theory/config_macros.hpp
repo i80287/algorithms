78b8b0af51f96384e493b6c41b7f1d824cf8fca4
i80287 <addmlbx@gmail.com>
2024-01-14 18:02:21 +0300

    add restrict_qualifier macro to the config_macros.hpp

--- number_theory/config_macros.hpp
+++ number_theory/config_macros.hpp
@@ -1,6 +1,19 @@
 #ifndef CONFIG_MACROS_HPP
 #define CONFIG_MACROS_HPP 1
 
+/**
+ * Restrict qualifier for the C++ (C has `restrict` keyword since C99)
+ */
+#if defined(__GNUC__)
+#define restrict_qualifier __restrict__
+#elif defined(__clang__)
+#define restrict_qualifier __restrict__
+#elif defined(_MSC_VER)
+#define restrict_qualifier __restrict
+#else
+#define restrict_qualifier
+#endif
+
 #if __cplusplus >= 202302L
 #define attribute_assume(expr) [[assume(expr)]]
 #elif defined(__GNUC__) && __GNUC__ >= 13 && !defined(__clang__)
