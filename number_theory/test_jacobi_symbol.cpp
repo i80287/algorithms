84ebb064cd5fc2baa92da95c95aaec7aece7117f
i80287 <addmlbx@gmail.com>
2024-01-16 02:16:12 +0300

    add constexpr kMaxFibonacciNonOverflowN

--- number_theory/test_jacobi_symbol.cpp
+++ number_theory/test_jacobi_symbol.cpp
@@ -1,12 +1,16 @@
 #include <cassert>
 #include <cinttypes>
 
+#include "config_macros.hpp"
+#include "fibonacci_fast.hpp"
 #include "jacobi_symbol.hpp"
 
 // Link with -lgmp and -lgmpxx
 #include <gmpxx.h>
 
-static const int32_t jcb[30][30] = {
+/// @brief Kronecker symbols (n/k) for 1 <= n <= 30 and 1 <= k <= 30
+///        Here (n/k) = krnk[n - 1][k - 1] (using zero indexing notation)
+static const int32_t krnk[30][30] = {
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
      1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
     {1, 0, -1, 0,  -1, 0,  1, 0, 1, 0, -1, 0,  -1, 0,  1,
@@ -69,9 +73,11 @@ static const int32_t jcb[30][30] = {
      0, 1, 0, -1, 0, 0, 0,  1, 0, 0, 0, 0, 0, 1, 0},
 };
 
-static const uint32_t primes[30] = {
+/// @brief First 30 odd prime numbers
+static const uint32_t odd_primes[30] = {
     3,  5,  7,  11, 13, 17, 19, 23, 29, 31,  37,  41,  43,  47,  53,
     59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127};
+/// @brief Legendre symbols (a/p) for 1 <= a <= 30 and 3 <= p <= 127, p is prime
 static const int32_t lgnr[30][30] = {
     {1, -1, 0, 1, -1, 0, 1, -1, 0, 1, -1, 0, 1, -1, 0,
      1, -1, 0, 1, -1, 0, 1, -1, 0, 1, -1, 0, 1, -1, 0},
@@ -134,6 +140,148 @@ static const int32_t lgnr[30][30] = {
     {1, 1, -1, 1, -1, -1, -1, 1,  1,  -1, 1, -1, 1,  -1, 1,
      1, 1, 1,  1, -1, 1,  1,  -1, -1, 1,  1, -1, -1, -1, 1},
 };
+
+static void CheckJacobi(uint32_t a, uint32_t n, int32_t real_jacobi) noexcept {
+    // Special checker for a < 2^31 and n < 2^31
+    assert(JacobiSymbol(a, n) == real_jacobi);
+    assert(JacobiSymbol(int32_t(a), n) == real_jacobi);
+    assert(JacobiSymbol(a, int32_t(n)) == real_jacobi);
+    assert(JacobiSymbol(int32_t(a), int32_t(n)) == real_jacobi);
+
+    assert(JacobiSymbol(uint64_t(a), uint64_t(n)) == real_jacobi);
+    assert(JacobiSymbol(uint64_t(a), int64_t(n)) == real_jacobi);
+    assert(JacobiSymbol(int64_t(a), uint64_t(n)) == real_jacobi);
+    assert(JacobiSymbol(int64_t(a), int64_t(n)) == real_jacobi);
+
+    // a >= 0 => (a/-1) = 1 => (a/n) = (a/-n)
+    assert(JacobiSymbol(a, -int32_t(n)) == real_jacobi);
+    assert(JacobiSymbol(int32_t(a), -int32_t(n)) == real_jacobi);
+    assert(JacobiSymbol(int64_t(a), -int64_t(n)) == real_jacobi);
+    assert(JacobiSymbol(uint64_t(a), -int64_t(n)) == real_jacobi);
+}
+
+template <uint16_t kLen>
+static void CheckJacobiBasic() noexcept {
+    printf("Started tests in %s\n", FUNCTION_MACRO);
+
+    for (uint32_t n = 1; n <= 30; n++) {
+        for (uint32_t k = 1; k <= 30; k++) {
+            CheckJacobi(k, n, krnk[n - 1][k - 1]);
+        }
+    }
+
+    for (int32_t k = -int32_t(kLen); k <= int32_t(kLen); k++) {
+        const int32_t b = k == 1 || k == -1;
+        assert(JacobiSymbol(k, int32_t(0)) == b);
+        assert(JacobiSymbol(k, uint32_t(0)) == b);
+        assert(JacobiSymbol(int64_t(k), int64_t(0)) == b);
+        assert(JacobiSymbol(int64_t(k), uint64_t(0)) == b);
+    }
+
+    for (size_t i = 0; i < 30; i++) {
+        uint32_t p = odd_primes[i];
+        for (uint32_t a = 1; a <= 30; a++) {
+            CheckJacobi(a, p, lgnr[i][a - 1]);
+        }
+    }
+
+    // Check some properties of the Legendre/Kronecker/Jacobi symbol
+    for (int32_t a = -int32_t(kLen); a <= kLen; a++) {
+        for (int32_t n = -int32_t(kLen); a <= kLen; a++) {
+            uint32_t j_abs = uint32_t(std::abs(JacobiSymbol(a, n)));
+            assert(j_abs == uint32_t(std::gcd(a, n) == 1));
+        }
+    }
+
+    for (uint32_t p : odd_primes) {
+        assert(JacobiSymbol(p, p) == 0);
+        for (uint32_t q : odd_primes) {
+            if (unlikely(p == q)) {
+                continue;
+            }
+            int32_t j_p_q = JacobiSymbol(p, q);
+            int32_t j_q_p = JacobiSymbol(q, p);
+            uint32_t p12_q12_pow = ((p - 1) / 2) * ((q - 1) / 2);
+            // (-1)^{ ((p - 1) / 2) * ((q - 1) / 2) }
+            int32_t to_p12_q12_pow = 1 - int32_t((p12_q12_pow % 2) * 2);
+            assert(j_p_q * j_q_p == to_p12_q12_pow);
+        }
+
+        int32_t j_m1_p = JacobiSymbol(-1, p);
+        int32_t j_2_p = JacobiSymbol(2u, p);
+        switch (p % 8) {
+            case 1:
+                assert(j_m1_p == 1);
+                assert(j_2_p == 1);
+                break;
+            case 3:
+                assert(j_m1_p == -1);
+                assert(j_2_p == -1);
+                break;
+            case 5:
+                assert(j_m1_p == 1);
+                assert(j_2_p == -1);
+                break;
+            case 7:
+                assert(j_m1_p == -1);
+                assert(j_2_p == 1);
+                break;
+        }
+
+        int32_t j_3_p = JacobiSymbol(3u, p);
+        switch (p % 12) {
+            case 1:
+            case 11:
+                assert(j_3_p == 1);
+                break;
+            case 5:
+            case 7:
+                assert(j_3_p == -1);
+                break;
+        }
+
+        int32_t j_5_p = JacobiSymbol(5u, p);
+        uint32_t j_5_p_mod_p = uint32_t(j_5_p);
+        switch (p % 5) {
+            case 1:
+            case 4:
+                assert(j_5_p == 1);
+                break;
+            case 2:
+            case 3:
+                assert(j_5_p == -1);
+                j_5_p_mod_p = p - 1;
+                break;
+            case 0:
+                assert(p == 5);
+                assert(j_5_p == 0);
+                break;
+        }
+
+        if (p <= kMaxFibonacciNonOverflowN) {
+            if constexpr (fibonacci_num(1) == 1 && fibonacci_num(2) == 1) {
+                assert(fibonacci_num(p) % p == j_5_p_mod_p);
+            } else {
+                assert(fibonacci_num(p - 1) % p == j_5_p_mod_p);
+            }
+        }
+
+        for (uint32_t a = 0; a <= kLen; a++) {
+            int32_t j_a_p = JacobiSymbol(a, p);
+            assert(j_a_p == -1 || j_a_p == 0 || j_a_p == 1);
+            ATTRIBUTE_ASSUME(j_a_p == -1 || j_a_p == 0 || j_a_p == 1);
+            uint32_t j_a_p_mod_p = j_a_p == -1 ? p - 1 : uint32_t(j_a_p);
+            uint32_t a_p12 = math_utils::bin_pow_mod(a, (p - 1) / 2, p);
+            assert(j_a_p_mod_p == a_p12);
+            for (uint32_t b = 0; b <= kLen; b++) {
+                int32_t j_b_p = JacobiSymbol(b, p);
+                assert(a % p != b % p || j_a_p == j_b_p);
+                int32_t j_ab_p = JacobiSymbol(a * b, p);
+                assert(j_ab_p == j_a_p * j_b_p);
+            }
+        }
+    }
+}
 
 static void CheckJacobi(int32_t i, int32_t j, const mpz_class& n1,
                         const mpz_class& n2) noexcept {
@@ -225,6 +373,7 @@ static void CheckJacobi(int64_t i, uint64_t j, const mpz_class& n1,
 
 template <uint16_t kLen>
 static void GMPCheckJacobiI32() {
+    printf("Started tests in %s\n", FUNCTION_MACRO);
     mpz_class n1;
     mpz_class n2;
 
@@ -247,6 +396,7 @@ static void GMPCheckJacobiI32() {
 
 template <uint16_t kLen>
 static void GMPCheckJacobiI64() {
+    printf("Started tests in %s\n", FUNCTION_MACRO);
     mpz_class n1;
     mpz_class n2;
 
@@ -269,6 +419,7 @@ static void GMPCheckJacobiI64() {
 
 template <uint16_t kLen>
 static void GMPCheckJacobiU32() {
+    printf("Started tests in %s\n", FUNCTION_MACRO);
     mpz_class n1;
     mpz_class n2;
 
@@ -291,6 +442,7 @@ static void GMPCheckJacobiU32() {
 
 template <uint16_t kLen>
 static void GMPCheckJacobiU64() {
+    printf("Started tests in %s\n", FUNCTION_MACRO);
     mpz_class n1;
     mpz_class n2;
 
@@ -305,7 +457,8 @@ static void GMPCheckJacobiU64() {
     n1.set_str("18446744073709551615", 10);
     for (uint64_t i = UINT64_MAX; i >= UINT64_MAX - uint64_t(kLen); --i, --n1) {
         n2.set_str("18446744073709551615", 10);
-        for (uint64_t j = UINT64_MAX; j >= UINT64_MAX - uint64_t(kLen); --j, --n2) {
+        for (uint64_t j = UINT64_MAX; j >= UINT64_MAX - uint64_t(kLen);
+             --j, --n2) {
             CheckJacobi(i, j, n1, n2);
         }
     }
@@ -313,6 +466,7 @@ static void GMPCheckJacobiU64() {
 
 template <uint16_t kLen>
 static void GMPCheckJacobiU32I32() {
+    printf("Started tests in %s\n", FUNCTION_MACRO);
     mpz_class n1;
     mpz_class n2;
 
@@ -347,6 +501,7 @@ static void GMPCheckJacobiU32I32() {
 
 template <uint16_t kLen>
 static void GMPCheckJacobiU64I64() {
+    printf("Started tests in %s\n", FUNCTION_MACRO);
     mpz_class n1;
     mpz_class n2;
 
@@ -359,7 +514,8 @@ static void GMPCheckJacobiU64I64() {
         }
 
         n2.set_str("18446744073709551615", 10);
-        for (uint64_t j = UINT64_MAX; j >= UINT64_MAX - uint64_t(kLen); --j, --n2) {
+        for (uint64_t j = UINT64_MAX; j >= UINT64_MAX - uint64_t(kLen);
+             --j, --n2) {
             CheckJacobi(i, j, n1, n2);
             CheckJacobi(j, i, n2, n1);
         }
@@ -374,7 +530,8 @@ static void GMPCheckJacobiU64I64() {
         }
 
         n2.set_str("18446744073709551615", 10);
-        for (uint64_t j = UINT64_MAX; j >= UINT64_MAX - uint64_t(kLen); --j, --n2) {
+        for (uint64_t j = UINT64_MAX; j >= UINT64_MAX - uint64_t(kLen);
+             --j, --n2) {
             CheckJacobi(i, j, n1, n2);
             CheckJacobi(j, i, n2, n1);
         }
@@ -382,48 +539,8 @@ static void GMPCheckJacobiU64I64() {
 }
 
 int main() {
-    for (uint32_t n = 1; n <= 30; n++) {
-        for (uint32_t k = 1; k <= 30; k++) {
-            int32_t j = jcb[n - 1][k - 1];
-            assert(JacobiSymbol(k, n) == j);
-
-            assert(JacobiSymbol(int64_t(k), int64_t(n)) == j);
-            assert(JacobiSymbol(uint64_t(k), int64_t(n)) == j);
-            assert(JacobiSymbol(int64_t(k), uint64_t(n)) == j);
-            assert(JacobiSymbol(uint64_t(k), uint64_t(n)) == j);
-
-            assert(JacobiSymbol(k, -int32_t(n)) == j);
-            assert(JacobiSymbol(int64_t(k), -int64_t(n)) == j);
-            assert(JacobiSymbol(uint64_t(k), -int64_t(n)) == j);
-        }
-    }
-
-    for (int32_t k = -100; k <= 100; k++) {
-        const int32_t b = k == 1 || k == -1;
-        assert(JacobiSymbol(k, 0) == b);
-        assert(JacobiSymbol(k, uint32_t(0)) == b);
-        assert(JacobiSymbol(int64_t(k), int64_t(0)) == b);
-        assert(JacobiSymbol(int64_t(k), uint64_t(0)) == b);
-    }
-
-    for (size_t i = 0; i < 30; i++) {
-        uint32_t p = primes[i];
-        for (uint32_t a = 1; a <= 30; a++) {
-            int32_t l = lgnr[i][a - 1];
-            assert(JacobiSymbol(a, p) == l);
-
-            assert(JacobiSymbol(int64_t(a), int64_t(p)) == l);
-            assert(JacobiSymbol(uint64_t(a), int64_t(p)) == l);
-            assert(JacobiSymbol(int64_t(a), uint64_t(p)) == l);
-            assert(JacobiSymbol(uint64_t(a), uint64_t(p)) == l);
-
-            assert(JacobiSymbol(a, -int32_t(p)) == l);
-            assert(JacobiSymbol(int64_t(a), int64_t(-int32_t(p))) == l);
-            assert(JacobiSymbol(uint64_t(a), int64_t(-int32_t(p))) == l);
-        }
-    }
-
-    constexpr uint16_t kLen = 2048;
+    constexpr uint16_t kLen = 2000;
+    CheckJacobiBasic<kLen>();
     GMPCheckJacobiI32<kLen>();
     GMPCheckJacobiI64<kLen>();
     GMPCheckJacobiU32<kLen>();
