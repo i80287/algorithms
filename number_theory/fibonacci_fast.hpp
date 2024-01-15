fcbfb9f65276a8ac58763da7fe89f98bec7be099
i80287 <addmlbx@gmail.com>
2024-01-16 00:50:04 +0300

    update codestyle in fibonacci_fast.hpp

--- number_theory/fibonacci_fast.hpp
+++ number_theory/fibonacci_fast.hpp
@@ -2,10 +2,10 @@
 
 constexpr void matrix_mul(uint64_t m1[2][2], const uint64_t m2[2][2]) noexcept {
     const uint64_t tmp[2][2] = {
-        m1[0][0] * m2[0][0] + m1[0][1] * m2[1][0],
-        m1[0][0] * m2[0][1] + m1[0][1] * m2[1][1],
-        m1[1][0] * m2[0][0] + m1[1][1] * m2[1][0],
-        m1[1][0] * m2[0][1] + m1[1][1] * m2[1][1],
+        {m1[0][0] * m2[0][0] + m1[0][1] * m2[1][0],
+         m1[0][0] * m2[0][1] + m1[0][1] * m2[1][1]},
+        {m1[1][0] * m2[0][0] + m1[1][1] * m2[1][0],
+         m1[1][0] * m2[0][1] + m1[1][1] * m2[1][1]},
     };
     m1[0][0] = tmp[0][0];
     m1[0][1] = tmp[0][1];
@@ -15,12 +15,12 @@ constexpr void matrix_mul(uint64_t m1[2][2], const uint64_t m2[2][2]) noexcept {
 
 constexpr uint64_t fibonacci_num(uint32_t n) noexcept {
     uint64_t p[2][2] = {
-        0, 1,
-        1, 1,
+        {0, 1},
+        {1, 1},
     };
     uint64_t fibmatrix[2][2] = {
-        1, 0,
-        0, 1,
+        {1, 0},
+        {0, 1},
     };
 
     while (true) {
