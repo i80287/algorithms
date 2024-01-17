8c9c931163bbfe6c86392c4621bbfd1cc075e637
i80287 <addmlbx@gmail.com>
2024-01-17 19:48:34 +0300

    update tf-idf + actrie search algorithm

--- TF_IDF_ACTRIE/search_lib.hpp
+++ TF_IDF_ACTRIE/search_lib.hpp
@@ -1,7 +1,7 @@
 #ifndef _SEARCH_LIB_H_
 #define _SEARCH_LIB_H_ 1
 
-#include <cstdint>     // uint64_t
+#include <cstdint>     // std::size_t
 #include <string_view> // std::string_view
 #include <vector>      // std::vector
 
@@ -9,7 +9,7 @@ namespace search_lib {
     using std::string_view;
 
     template <bool IsExactWordsMatching = true>
-    std::vector<string_view> Search(string_view text, string_view query, uint64_t result_size);
+    std::vector<string_view> Search(string_view text, string_view query, std::size_t result_size);
 } // namespace search_lib
 
 #endif
