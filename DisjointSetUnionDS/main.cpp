ed9e5434255c474b24aabd18bd4a2565a77a90af
i80287 <addmlbx@gmail.com>
2024-01-17 13:52:34 +0300

    update DSU

--- DisjointSetUnionDS/main.cpp
+++ DisjointSetUnionDS/main.cpp
@@ -2,120 +2,122 @@
 #include <cstdint>  // size_t, int64_t
 #include <cstring>  // std::memset
 #if __cplusplus >= 202002L
-#include <concepts> // std::convertible_to
+#include <concepts>  // std::convertible_to
 #endif
 #include <utility>  // std::move
 #include <vector>   // std::vector
 
-template<typename NodeType>
-class DSUMethodsBase {
-public:
+// O(log*(n)) [log* - iterated logarithm, log*(n) = log2(log2(log2(...log2(n),
+// until log2 > 0))), log*(65536) = 5, log*(2^65536) = 6]
+template <typename NodeType>
 #if __cplusplus >= 202002L
-    static_assert(requires (NodeType* node) { { node->parent_ } -> std::convertible_to<NodeType*>; });
-#endif
-
-    // O(log*(n)) [log* - iterated logarithm, log*(n) = log2(log2(log2(...log2(n), until log2 > 0))), log*(65536) = 5, log*(2^65536) = 6]
-    static constexpr NodeType* FindRoot(NodeType* node) noexcept {
-        NodeType* current_node = node;
-        assert(current_node != nullptr);
-        while (current_node->parent_ != nullptr) {
-            assert(current_node != current_node->parent_);
-            current_node = current_node->parent_;
-            assert(current_node != nullptr);
-        }
-
-        // Now 'current_node' points to the root
-        while (node != current_node)  {
-            NodeType* next = node->parent_;
-            node->parent_ = current_node;
-            node = next;
-        }
-
-        return current_node;
+    requires requires(NodeType* node) {
+        { node->parent_ } -> std::convertible_to<NodeType*>;
     }
-};
+#endif
+static NodeType* FindRoot(NodeType* node) noexcept {
+    NodeType* current_node = node;
+    assert(current_node != nullptr);
+    while (current_node->parent_ != nullptr) {
+        assert(current_node != current_node->parent_);
+        current_node = current_node->parent_;
+        assert(current_node != nullptr);
+    }
 
-// Node with rank heuristic
-struct DSUTreeNode {
-    DSUTreeNode* parent_;
-    size_t rank_;
-};
+    // Now 'current_node' points to the root
+    while (node != current_node) {
+        NodeType* next = node->parent_;
+        node->parent_ = current_node;
+        node = next;
+    }
+
+    return current_node;
+}
 
 /// @brief See also https://www.youtube.com/watch?v=KFcpDTpoixo
-class DisjointSetUnion : DSUMethodsBase<DSUTreeNode> {
+class DisjointSetUnion {
 public:
+    // Node with rank heuristic
+    struct DSUTreeNode {
+        DSUTreeNode* parent_;
+        size_t rank_;
+    };
+
     DSUTreeNode* nodes_ = nullptr;
     size_t nodes_count_ = 0;
 
     DisjointSetUnion() = delete;
 
     // O(n)
-    inline explicit DisjointSetUnion(size_t nodes_count) noexcept(false) : nodes_count_{nodes_count} {
+    explicit DisjointSetUnion(size_t nodes_count)
+        : nodes_count_{nodes_count} {
         if (nodes_count) {
-            nodes_ = static_cast<DSUTreeNode*>(operator new(sizeof(DSUTreeNode) * nodes_count));
+            nodes_ = static_cast<DSUTreeNode*>(operator new(
+                sizeof(DSUTreeNode) * nodes_count));
             std::memset(nodes_, 0, sizeof(DSUTreeNode) * nodes_count);
         }
     }
 
     // O(n)
-    inline DisjointSetUnion(const DisjointSetUnion& other) noexcept(false) {
-        *this = other;
+    DisjointSetUnion(const DisjointSetUnion& other) : nodes_count_(other.nodes_count_) {
+        const size_t count = nodes_count_;
+        if (count == 0) {
+            return;
+        }
+
+        DSUTreeNode* const this_first_node = nodes_ =
+            static_cast<DSUTreeNode*>(operator new(sizeof(DSUTreeNode) *
+                                                   count));
+
+        for (size_t i = 0; i < count; ++i) {
+            const DSUTreeNode* other_i_node_parent = other.nodes_[i].parent_;
+            size_t parent_offset = static_cast<size_t>(other_i_node_parent - other.nodes_);
+            this_first_node[i].parent_ = other_i_node_parent ? this_first_node + parent_offset : nullptr;
+            this_first_node[i].rank_ = other.nodes_[i].rank_;
+        }
     }
 
     // O(n)
-    DisjointSetUnion& operator=(const DisjointSetUnion& other) noexcept(false) {
-        if (this == &other) {
-            return *this;
-        }
-
-        this->~DisjointSetUnion();
-
-        const size_t count = nodes_count_ = other.nodes_count_;
-        nodes_ = nullptr;
-        if (count == 0) {
-            return *this;
-        }
-
-        DSUTreeNode* const this_first_node = nodes_ = static_cast<DSUTreeNode*>(operator new(sizeof(DSUTreeNode) * count));
-        const DSUTreeNode* const other_first_node = other.nodes_;
-        const ptrdiff_t parent_offset = this_first_node - other_first_node;
-
-        for (size_t i = 0; i < count; ++i) {
-            const DSUTreeNode* other_i_node_parent = other_first_node[i].parent_;
-            this_first_node[i].parent_ = other_i_node_parent ? const_cast<DSUTreeNode*>(other_i_node_parent + parent_offset) : nullptr;
-            this_first_node[i].rank_ = other_first_node[i].rank_;
-        }
-
-        return *this;
+    DisjointSetUnion& operator=(const DisjointSetUnion& other) {
+        return *this = DisjointSetUnion(other);
     }
 
     // O(1)
-    inline constexpr DisjointSetUnion(DisjointSetUnion&& other) noexcept {
-        *this = std::move(other);
-    }
-
-    // O(1)
-    inline constexpr DisjointSetUnion& operator=(DisjointSetUnion&& other) noexcept {
-        nodes_ = other.nodes_;
-        nodes_count_ = other.nodes_count_;
+    constexpr DisjointSetUnion(DisjointSetUnion&& other) noexcept
+        : nodes_(other.nodes_), nodes_count_(other.nodes_count_) {
         other.nodes_ = nullptr;
         other.nodes_count_ = 0;
+    }
+
+    // O(1)
+    DisjointSetUnion& operator=(DisjointSetUnion&& other) noexcept {
+        auto nodes = other.nodes_;
+        auto nodes_count = other.nodes_count_;
+        this->~DisjointSetUnion();
+        other.nodes_ = nullptr;
+        other.nodes_count_ = 0;
+        nodes_ = nodes;
+        nodes_count_ = nodes_count;
         return *this;
     }
 
     // O(log*(n))
-    inline constexpr bool Equivalent(size_t node_x_index, size_t node_y_index) const noexcept {
+    bool Equivalent(size_t node_x_index,
+                                     size_t node_y_index) const noexcept {
         assert(node_x_index < nodes_count_ && node_y_index < nodes_count_);
-        return FindRoot(&nodes_[node_x_index]) == FindRoot(&nodes_[node_y_index]);
+        return FindRoot(&nodes_[node_x_index]) ==
+               FindRoot(&nodes_[node_y_index]);
     }
 
     // O(log*(n))
-    constexpr void Unite(size_t node_x_index, size_t node_y_index) const noexcept {
+    void Unite(size_t node_x_index,
+                         size_t node_y_index) const noexcept {
         assert(node_x_index < nodes_count_ && node_y_index < nodes_count_);
         DSUTreeNode* node_x_root_ptr = FindRoot(&nodes_[node_x_index]);
         DSUTreeNode* node_y_root_ptr = FindRoot(&nodes_[node_y_index]);
         if (node_x_root_ptr == node_y_root_ptr) {
-            // Do not unite already united nodes so that for each root node: root_node->parent_ == nullptr
+            // Do not unite already united nodes so that for each root node:
+            // root_node->parent_ == nullptr
             return;
         }
 
@@ -123,12 +125,10 @@ public:
         size_t node_y_root_rank = node_y_root_ptr->rank_;
         if (node_x_root_rank > node_y_root_rank) {
             node_y_root_ptr->parent_ = node_x_root_ptr;
-        }
-        else if (node_x_root_rank != node_y_root_rank) {
+        } else if (node_x_root_rank != node_y_root_rank) {
             // node_x_root_rank < node_y_root_rank
             node_x_root_ptr->parent_ = node_y_root_ptr;
-        }
-        else {
+        } else {
             // node_x_root_rank == node_y_root_rank
             node_x_root_ptr->parent_ = node_y_root_ptr;
             node_y_root_ptr->rank_++;
@@ -136,10 +136,9 @@ public:
     }
 
     // O(n)
-    inline ~DisjointSetUnion() {
-        operator delete (nodes_);
+    ~DisjointSetUnion() {
+        operator delete(nodes_);
         nodes_ = nullptr;
-        nodes_count_ = 0;
     }
 };
 
@@ -150,8 +149,9 @@ struct WeightedDSUTreeNode {
     int64_t weight_;
 };
 
-/// @brief See also class DisjointSetUnion and https://youtu.be/MmemGjxsZTc?si=NHMBw-KJmxeXvkNA
-class WeightedDisjointSetUnion : DSUMethodsBase<WeightedDSUTreeNode> {
+/// @brief See also class DisjointSetUnion and
+/// https://youtu.be/MmemGjxsZTc?si=NHMBw-KJmxeXvkNA
+class WeightedDisjointSetUnion {
 public:
     WeightedDSUTreeNode* nodes_ = nullptr;
     size_t nodes_count_ = 0;
@@ -159,85 +159,96 @@ public:
     WeightedDisjointSetUnion() = delete;
 
     // O(n)
-    inline explicit WeightedDisjointSetUnion(size_t nodes_count) noexcept(false) : nodes_count_{nodes_count} {
+    explicit WeightedDisjointSetUnion(size_t nodes_count) noexcept(false)
+        : nodes_count_{nodes_count} {
         if (nodes_count) {
-            nodes_ = static_cast<WeightedDSUTreeNode*>(operator new(sizeof(WeightedDSUTreeNode) * nodes_count));
+            nodes_ = static_cast<WeightedDSUTreeNode*>(operator new(
+                sizeof(WeightedDSUTreeNode) * nodes_count));
             std::memset(nodes_, 0, sizeof(WeightedDSUTreeNode) * nodes_count);
         }
     }
 
     // O(n)
-    inline explicit WeightedDisjointSetUnion(const std::vector<int64_t>& weights) noexcept(false) : nodes_count_{weights.size()} {
+    explicit WeightedDisjointSetUnion(
+        const std::vector<int64_t>& weights) noexcept(false)
+        : nodes_count_{weights.size()} {
         if (nodes_count_) {
-            nodes_ = static_cast<WeightedDSUTreeNode*>(operator new(sizeof(WeightedDSUTreeNode) * nodes_count_));
+            nodes_ = static_cast<WeightedDSUTreeNode*>(operator new(
+                sizeof(WeightedDSUTreeNode) * nodes_count_));
             std::memset(nodes_, 0, sizeof(WeightedDSUTreeNode) * nodes_count_);
             WeightedDSUTreeNode* nodes_iter = nodes_;
-            for (auto iter = weights.begin(), end = weights.end(); iter != end; ++iter, ++nodes_iter) {
+            for (auto iter = weights.begin(), end = weights.end(); iter != end;
+                 ++iter, ++nodes_iter) {
                 nodes_iter->weight_ = *iter;
             }
         }
     }
 
     // O(n)
-    inline WeightedDisjointSetUnion(const WeightedDisjointSetUnion& other) noexcept(false) {
-        *this = other;
+    WeightedDisjointSetUnion(const WeightedDisjointSetUnion& other)
+        : nodes_count_(other.nodes_count_) {
+        const size_t count = nodes_count_;
+        if (count == 0) {
+            return;
+        }
+
+        WeightedDSUTreeNode* const this_first_node = nodes_ =
+            static_cast<WeightedDSUTreeNode*>(operator new(
+                sizeof(WeightedDSUTreeNode) * count));
+
+        for (size_t i = 0; i < count; ++i) {
+            const WeightedDSUTreeNode* other_i_node_parent =
+                other.nodes_[i].parent_;
+            size_t parent_offset =
+                static_cast<size_t>(other_i_node_parent - other.nodes_);
+            this_first_node[i].parent_ =
+                other_i_node_parent ? this_first_node + parent_offset : nullptr;
+            this_first_node[i].rank_ = other.nodes_[i].rank_;
+            this_first_node[i].weight_ = other.nodes_[i].weight_;
+        }
     }
 
     // O(n)
-    WeightedDisjointSetUnion& operator=(const WeightedDisjointSetUnion& other) noexcept(false) {
-        if (this == &other) {
-            return *this;
-        }
-
-        this->~WeightedDisjointSetUnion();
-
-        const size_t count = nodes_count_ = other.nodes_count_;
-        nodes_ = nullptr;
-        if (count == 0) {
-            return *this;
-        }
-
-        WeightedDSUTreeNode* const this_first_node = nodes_ = static_cast<WeightedDSUTreeNode*>(operator new(sizeof(WeightedDSUTreeNode) * count));
-        const WeightedDSUTreeNode* const other_first_node = other.nodes_;
-        const ptrdiff_t parent_offset = this_first_node - other_first_node;
-
-        for (size_t i = 0; i < count; ++i) {
-            const WeightedDSUTreeNode* other_i_node_parent = other_first_node[i].parent_;
-            this_first_node[i].parent_ = other_i_node_parent ? const_cast<WeightedDSUTreeNode*>(other_i_node_parent + parent_offset) : nullptr;
-            this_first_node[i].rank_ = other_first_node[i].rank_;
-            this_first_node[i].weight_ = other_first_node[i].weight_;
-        }
-
-        return *this;
+    WeightedDisjointSetUnion& operator=(
+        const WeightedDisjointSetUnion& other) {
+        return *this = WeightedDisjointSetUnion(other);
     }
 
     // O(1)
-    inline constexpr WeightedDisjointSetUnion(WeightedDisjointSetUnion&& other) noexcept {
-        *this = std::move(other);
-    }
-
-    // O(1)
-    inline constexpr WeightedDisjointSetUnion& operator=(WeightedDisjointSetUnion&& other) noexcept {
-        nodes_ = other.nodes_;
-        nodes_count_ = other.nodes_count_;
+    constexpr WeightedDisjointSetUnion(WeightedDisjointSetUnion&& other) noexcept
+        : nodes_(other.nodes_), nodes_count_(other.nodes_count_) {
         other.nodes_ = nullptr;
         other.nodes_count_ = 0;
+    }
+
+    // O(1)
+    WeightedDisjointSetUnion& operator=(WeightedDisjointSetUnion&& other) noexcept {
+        auto nodes = other.nodes_;
+        auto nodes_count = other.nodes_count_;
+        this->~WeightedDisjointSetUnion();
+        other.nodes_ = nullptr;
+        other.nodes_count_ = 0;
+        nodes_ = nodes;
+        nodes_count_ = nodes_count;
         return *this;
     }
 
     // O(log*(n))
-    inline constexpr bool Equivalent(size_t node_x_index, size_t node_y_index) const noexcept {
+    bool Equivalent(size_t node_x_index,
+                                     size_t node_y_index) const noexcept {
         assert(node_x_index < nodes_count_ && node_y_index < nodes_count_);
-        return FindRoot(&nodes_[node_x_index]) == FindRoot(&nodes_[node_y_index]);
+        return FindRoot(&nodes_[node_x_index]) ==
+               FindRoot(&nodes_[node_y_index]);
     }
 
     // O(log*(n))
-    constexpr void Unite(size_t node_x_index, size_t node_y_index) noexcept {
+    void Unite(size_t node_x_index, size_t node_y_index) noexcept {
         assert(node_x_index < nodes_count_ && node_y_index < nodes_count_);
         WeightedDSUTreeNode* node_x_root_ptr = FindRoot(&nodes_[node_x_index]);
         WeightedDSUTreeNode* node_y_root_ptr = FindRoot(&nodes_[node_y_index]);
         if (node_x_root_ptr == node_y_root_ptr) {
-            // Do not unite already united nodes so that for each root node: root_node->parent_ == nullptr
+            // Do not unite already united nodes so that for each root node:
+            // root_node->parent_ == nullptr
             return;
         }
 
@@ -246,13 +257,11 @@ public:
         if (node_x_root_rank > node_y_root_rank) {
             node_y_root_ptr->parent_ = node_x_root_ptr;
             node_x_root_ptr->weight_ += node_y_root_ptr->weight_;
-        }
-        else if (node_x_root_rank != node_y_root_rank) {
+        } else if (node_x_root_rank != node_y_root_rank) {
             // node_x_root_rank < node_y_root_rank
             node_x_root_ptr->parent_ = node_y_root_ptr;
             node_y_root_ptr->weight_ += node_x_root_ptr->weight_;
-        }
-        else {
+        } else {
             // node_x_root_rank == node_y_root_rank
             node_x_root_ptr->parent_ = node_y_root_ptr;
             node_y_root_ptr->weight_ += node_x_root_ptr->weight_;
@@ -261,26 +270,26 @@ public:
     }
 
     // O(log*(n))
-    inline constexpr int64_t GetWeightInSet(size_t node_index) const noexcept {
+    int64_t GetWeightInSet(size_t node_index) const noexcept {
         assert(node_index < nodes_count_);
         return FindRoot(&nodes_[node_index])->weight_;
     }
 
     // O(log*(n))
-    inline constexpr void AddWeightInSet(size_t node_index, int64_t delta) {
+    void AddWeightInSet(size_t node_index, int64_t delta) {
         assert(node_index < nodes_count_);
         FindRoot(&nodes_[node_index])->weight_ += delta;
     }
 
     // O(log*(n))
-    inline constexpr void SetWeightInSet(size_t node_index, int64_t weight) {
+    void SetWeightInSet(size_t node_index, int64_t weight) {
         assert(node_index < nodes_count_);
         FindRoot(&nodes_[node_index])->weight_ = weight;
     }
 
     // O(n)
-    inline ~WeightedDisjointSetUnion() {
-        operator delete (nodes_);
+    ~WeightedDisjointSetUnion() {
+        operator delete(nodes_);
         nodes_ = nullptr;
         nodes_count_ = 0;
     }
