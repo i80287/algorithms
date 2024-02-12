8369ba27c4e92d977776f7ecd44b32f45b67ae51
i80287 <addmlbx@gmail.com>
2024-02-12 09:47:29 +0300

    added eratosthene_sieve to the math_functions namespace

--- number_theory/eratosthene_sieve.cpp
+++ number_theory/eratosthene_sieve.cpp
@@ -1,67 +1,73 @@
-#include <vector>
 #include <bitset>
-#include <cstdint>
+#include <cassert>
 #include <cstddef>
-#include <cmath>
-#include <iostream>
+#include <cstdint>
+#include <vector>
+
 #include "math_functions.hpp"
 
-/// @brief Print all prime numbers in [2; N)
-/// @param N exclusive upper border
-void print_sieve1(size_t N) {
-    if (N <= 2) {
-        return;
-    }
+namespace math_functions {
 
-    std::vector<bool> primes(N, true);
-    primes[0] = primes[1] = false;
-    const size_t root = math_functions::isqrt(N);
+/// @brief Find all prime numbers in [2; n)
+/// @param N exclusive upper bound
+/// @return vector<bool>, for which bvec[n] == true \iff n is prime
+std::vector<bool> primes_sieve_as_bvector(size_t n) {
+    std::vector<bool> primes(n, true);
+    if (likely(n >= 2)) {
+        primes[0] = false;
+        primes[1] = false;
+    }
+    const size_t root = math_functions::isqrt(n);
     for (size_t i = 2; i <= root; ++i) {
         if (primes[i]) {
-            for (size_t j = uint32_t(i) * uint32_t(i); j < N; j += i) {
+            for (size_t j = uint32_t(i) * uint32_t(i); j < n; j += i) {
                 primes[j] = false;
             }
         }
     }
 
-    for (size_t i = 2; i < N; i++) {
-        if (primes[i])
-            std::cout << i << ' ';
-    }
+    return primes;
 }
 
-/// @brief Print all prime numbers in [2; N)
-/// @param N exclusive upper border
+/// @brief Find all prime numbers in [2; N)
+/// @tparam N exclusive upper bound
+/// @return bitset, for which bset[n] == true \iff n is prime
 template <size_t N>
-void print_sieve2() {
-    if constexpr (N <= 2) {
-        return;
-    }
-
-    static std::bitset<N> primes;
-    primes.set();
-    primes[0] = primes[1] = false;
-    constexpr size_t root = math_functions::isqrt(N);
-    for (size_t i = 2; i <= root; i++) {
-        if (primes[i]) {
-            for (size_t j = uint32_t(i) * uint32_t(i); j < N; j += i) {
-                primes[j] = false;
+std::bitset<N>& primes_sieve_as_bitset() noexcept {
+    static constinit std::bitset<N> primes;
+    if constexpr (N > 2) {
+        primes.set();
+        primes[0]             = false;
+        primes[1]             = false;
+        constexpr size_t root = math_functions::isqrt(N);
+        for (size_t i = 2; i <= root; i++) {
+            if (primes[i]) {
+                for (size_t j = uint32_t(i) * uint32_t(i); j < N; j += i) {
+                    primes[j] = false;
+                }
             }
         }
     }
 
-    for (size_t i = 2; i < N; i++) {
-        if (primes[i])
-            std::cout << i << ' ';
-    }
+    return primes;
 }
+
+}  // namespace math_functions
 
 int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-
-    print_sieve1(100);
-    std::cout << '\n';
-    print_sieve2<100>();
-    std::cout << std::endl;
+    constexpr size_t N                  = 100;
+    std::vector<bool> primes_as_bvector = math_functions::primes_sieve_as_bvector(N);
+    std::bitset<N>& primes_bset         = math_functions::primes_sieve_as_bitset<N>();
+    constexpr uint32_t primes[] = {2,  3,  5,  7,   11,  13,  17,  19,  23, 29, 31,
+                                   37, 41, 43, 47,  53,  59,  61,  67,  71, 73, 79,
+                                   83, 89, 97, 101, 103, 107, 109, 113, 127};
+    size_t i                    = 0;
+    for (uint32_t n = 0; n < N; n++) {
+        if (primes[i] < n) {
+            i++;
+        }
+        bool is_prime = primes[i] == n;
+        assert(primes_as_bvector[n] == is_prime);
+        assert(primes_bset[n] == is_prime);
+    }
 }
