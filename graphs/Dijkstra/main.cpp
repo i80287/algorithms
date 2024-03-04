817dadb24990747859235f649a12ff4e864d1d1d
i80287 <addmlbx@gmail.com>
2024-03-04 22:27:07 +0300

    .\graphs\Dijkstra\

--- graphs/Dijkstra/main.cpp
+++ graphs/Dijkstra/main.cpp
@@ -1,6 +1,6 @@
 #include <cassert>
-#include <cstdint>
 #include <cstddef>
+#include <cstdint>
 #include <vector>
 
 namespace dijkstra {
@@ -8,169 +8,150 @@ namespace dijkstra {
 using std::vector;
 using vertex_t = size_t;
 using weight_t = uint64_t;
-using graph_t = vector<vector<std::pair<vertex_t, weight_t>>>;
+using graph_t  = vector<vector<std::pair<vertex_t, weight_t>>>;
 
-inline constexpr weight_t kInfDist = weight_t(-1);
+inline constexpr weight_t kInfDist  = weight_t(-1);
 inline constexpr vertex_t kNoVertex = vertex_t(-1);
 
-class DHeap {
+class heap {
 public:
-    using Node = std::pair<weight_t, vertex_t>;
+    struct node_t {
+        weight_t dist;
+        vertex_t vertex;
+    };
 
-    static constexpr weight_t Dist(const Node& node) noexcept {
-        return node.first;
+    static constexpr heap from_graph(size_t graph_size, vertex_t start_vertex) {
+        std::vector<node_t> heap_nodes(graph_size);
+        for (vertex_t vertex = 1; size_t(vertex) < graph_size; vertex++) {
+            heap_nodes[vertex].dist   = kInfDist;
+            heap_nodes[vertex].vertex = vertex;
+        }
+        heap_nodes[0].dist              = 0;
+        heap_nodes[0].vertex            = start_vertex;
+        heap_nodes[start_vertex].vertex = 0;
+        return heap(std::move(heap_nodes));
     }
 
-    constexpr void reserve(size_t size) {
-        heap_nodes_.reserve(size);
-    }
+    constexpr void reserve(size_t size) { heap_nodes_.reserve(size); }
 
-    constexpr size_t size() const noexcept {
-        return heap_nodes_.size();
-    }
+    constexpr size_t size() const noexcept { return heap_nodes_.size(); }
 
-    constexpr bool empty() const noexcept {
-        return heap_nodes_.empty();
-    }
+    constexpr bool empty() const noexcept { return heap_nodes_.empty(); }
 
-    constexpr Node& Top() noexcept {
-        return heap_nodes_.front();
-    }
+    constexpr node_t& top() noexcept { return heap_nodes_.front(); }
 
-    constexpr const Node& Top() const noexcept {
-        return heap_nodes_.front();
-    }
+    constexpr const node_t& top() const noexcept { return heap_nodes_.front(); }
 
-    constexpr const vector<Node>& Nodes() const noexcept {
-        return heap_nodes_;
-    }
+    void push(weight_t v_dist, vertex_t vertex) {
+        // push elem to the end of the heap and sift upper.
 
-    constexpr vector<Node>& Nodes() noexcept {
-        return heap_nodes_;
-    }
-
-    void Add(weight_t dist, vertex_t vertex) {
-        // Add elem to the end of the heap and sift upper.
-
-        size_t elem_index = size();
-        size_t parent_index = ParentIndex(elem_index);
+        size_t elem_index   = size();
+        size_t parent_index = parentIndex(elem_index);
         heap_nodes_.emplace_back();
-        while (elem_index != 0 && Dist(heap_nodes_[parent_index]) > dist) {
+        while (elem_index != 0 && heap_nodes_[parent_index].dist > v_dist) {
             heap_nodes_[elem_index] = heap_nodes_[parent_index];
-            elem_index = parent_index;
-            parent_index = ParentIndex(elem_index);
+            elem_index              = parent_index;
+            parent_index            = parentIndex(elem_index);
         }
 
-        heap_nodes_[elem_index] = { dist, vertex };
+        heap_nodes_[elem_index] = {v_dist, vertex};
     }
 
-    void PopTop() noexcept {
+    void pop_top() noexcept {
         // Remove root, place last element on it's place
         // and return pyramide (heap) to the balanced state.
         assert(!empty());
-        Node sifting_elem = heap_nodes_.front() = heap_nodes_.back();
-        size_t parent_index = 0;
-        size_t son_index = LeftSonIndex(parent_index);
+        node_t sifting_elem = heap_nodes_.front() = heap_nodes_.back();
+        size_t parent_index                       = 0;
+        size_t son_index                          = leftSonIndex(parent_index);
         while (son_index + 1 < size()) {
-            if (Dist(heap_nodes_[son_index]) > Dist(heap_nodes_[son_index + 1])) {
+            if (heap_nodes_[son_index].dist > heap_nodes_[son_index + 1].dist) {
                 son_index++;
             }
 
-            if (Dist(sifting_elem) > Dist(heap_nodes_[son_index])) {
-                heap_nodes_[parent_index] = heap_nodes_[son_index];
-                heap_nodes_[son_index] = sifting_elem;
-                parent_index = son_index;
-                son_index = LeftSonIndex(parent_index);
-            }
-            else {
+            if (sifting_elem.dist <= heap_nodes_[son_index].dist) {
                 break;
             }
+            heap_nodes_[parent_index] = heap_nodes_[son_index];
+            heap_nodes_[son_index]    = sifting_elem;
+            parent_index              = son_index;
+            son_index                 = leftSonIndex(parent_index);
         }
 
         heap_nodes_.pop_back();
     }
 
-    void Build(size_t graph_size, vertex_t start_vertex) {
-        heap_nodes_.resize(graph_size);
-        for (vertex_t vertex = 1; size_t(vertex) < graph_size; vertex++) {
-            heap_nodes_[vertex].first = kInfDist;
-            heap_nodes_[vertex].second = vertex;
-        }
-        heap_nodes_[start_vertex].second = 0;
-        heap_nodes_[0].first = 0;
-        heap_nodes_[0].second = start_vertex;
+    constexpr void decrease_top_key(weight_t dist) noexcept { decrease_key(0, dist); }
+
+    constexpr void decrease_key(size_t node_index, weight_t dist) noexcept {
+        heap_nodes_[node_index].dist = dist;
+        rebalance_heap(node_index);
     }
 
-    constexpr void SetTopDist(weight_t dist) noexcept {
-        SetDist(0, dist);
-    }
+private:
+    constexpr heap(std::vector<node_t>&& heap_nodes) noexcept
+        : heap_nodes_(std::move(heap_nodes)) {}
 
-    constexpr void DecreaseDist(size_t node_index, weight_t dist) noexcept {
-        SetDist(node_index, Dist(heap_nodes_[node_index]) - dist);
-    }
-
-    constexpr void SetDist(size_t node_index, weight_t dist) noexcept {
-        heap_nodes_[node_index].first = dist;
-        RebalanceHeap(node_index);
-    }
-protected:
-    constexpr void RebalanceHeap(size_t index) noexcept {
-        size_t son_index = LeftSonIndex(index);
+    constexpr void rebalance_heap(size_t index) noexcept {
+        size_t son_index = leftSonIndex(index);
         if (son_index >= size()) {
             return;
         }
 
         size_t parent_index = index;
-        Node sifting_elem = heap_nodes_[index];
-        if (son_index + 1 != size() && Dist(heap_nodes_[son_index]) > Dist(heap_nodes_[son_index + 1])) {
+        node_t sifting_elem = heap_nodes_[index];
+        if (son_index + 1 != size() &&
+            heap_nodes_[son_index].dist > heap_nodes_[son_index + 1].dist) {
             son_index++;
         }
 
-        while (Dist(sifting_elem) > Dist(heap_nodes_[son_index])) {
+        while (sifting_elem.dist > heap_nodes_[son_index].dist) {
             heap_nodes_[parent_index] = heap_nodes_[son_index];
-            heap_nodes_[son_index] = sifting_elem;
+            heap_nodes_[son_index]    = sifting_elem;
 
             parent_index = son_index;
-            son_index = LeftSonIndex(son_index);
+            son_index    = leftSonIndex(son_index);
             if (son_index >= size()) {
                 return;
             }
 
-            if (son_index + 1 != size() && Dist(heap_nodes_[son_index]) > Dist(heap_nodes_[son_index + 1])) {
+            if (son_index + 1 != size() &&
+                heap_nodes_[son_index].dist > heap_nodes_[son_index + 1].dist) {
                 son_index++;
             }
         }
     }
 
-    static constexpr size_t ParentIndex(size_t node_index) noexcept {
-        return (node_index - 1) >> 1;
+    static constexpr size_t parentIndex(size_t node_index) noexcept {
+        return (node_index - 1) / 2;
     }
 
-    static constexpr size_t LeftSonIndex(size_t node_index) noexcept {
-        return node_index << 1 | 1;
+    static constexpr size_t leftSonIndex(size_t node_index) noexcept {
+        return node_index * 2 | 1;
     }
 
-    std::vector<Node> heap_nodes_;
+    std::vector<node_t> heap_nodes_;
 };
 
 /// @brief O(|V|^2 + |E|) = O(|V|^2), better use when |E| = \Theta(|V^2|)
 /// @param g graph = (V, E) with weight function
-/// @param dist 
-/// @param ancestors 
+/// @param dist
+/// @param ancestors
 /// @param from start vertex, 0 <= from < |V|
-static void FindShortestPathsHighDenisty(const graph_t& g, vector<weight_t>& dist, vector<vertex_t>& ancestors, uint32_t from) {
+static void find_shr_pths_highdensity(const graph_t& g, vector<weight_t>& dist,
+                                      vector<vertex_t>& ancestors, uint32_t from) {
     const size_t n = g.size();
     assert(dist.size() == n && ancestors.size() == n);
     assert(from < n);
     vector<bool> visited(n);
     dist[from] = 0;
-    while(true) {
-        size_t vertex = n;
+    while (true) {
+        size_t vertex     = n;
         weight_t min_dist = kInfDist;
         for (size_t v = 0; v < n; v++) {
             if (!visited[v] && dist[v] < min_dist) {
                 min_dist = dist[v];
-                vertex = v;
+                vertex   = v;
             }
         }
 
@@ -181,7 +162,7 @@ static void FindShortestPathsHighDenisty(const graph_t& g, vector<weight_t>& dis
         for (auto [neighbour, weight] : g[vertex]) {
             weight_t path_via_vertex = min_dist + weight;
             if (dist[neighbour] > path_via_vertex) {
-                dist[neighbour] = path_via_vertex;
+                dist[neighbour]      = path_via_vertex;
                 ancestors[neighbour] = vertex_t(vertex);
             }
         }
@@ -192,25 +173,25 @@ static void FindShortestPathsHighDenisty(const graph_t& g, vector<weight_t>& dis
 
 /// @brief O(|V| + |E| log|E|) = O(|E| log|V|), better use when |E| = \O(|V^2| / log|V|)
 /// @param g graph = (V, E) with weight function
-/// @param dist 
-/// @param ancestors 
+/// @param dist
+/// @param ancestors
 /// @param from start vertex, 0 <= from < |V|
-static void FindShortestPathsLowDenisty(const graph_t& g, vector<weight_t>& dist, vector<vertex_t>& ancestors, uint32_t from) {
+static void find_shr_pths_lowdensity(const graph_t& g, vector<weight_t>& dist,
+                                     vector<vertex_t>& ancestors, uint32_t from) {
     const size_t n = g.size();
     assert(dist.size() == n && ancestors.size() == n);
     assert(from < n);
     dist[from] = 0;
     vector<bool> visited(n);
-    DHeap not_visited;
-    not_visited.Build(n, from);
+    heap not_visited = heap::from_graph(n, from);
     assert(!not_visited.empty());
     while (true) {
         weight_t vertex_dist;
         vertex_t vertex;
         do {
-            vertex_dist = not_visited.Top().first;
-            vertex = not_visited.Top().second;
-            not_visited.PopTop();
+            vertex_dist = not_visited.top().dist;
+            vertex      = not_visited.top().vertex;
+            not_visited.pop_top();
         } while (visited[vertex] && !not_visited.empty());
 
         if (vertex_dist == kInfDist || not_visited.empty()) {
@@ -220,9 +201,9 @@ static void FindShortestPathsLowDenisty(const graph_t& g, vector<weight_t>& dist
         for (auto [neighbour, weight] : g[vertex]) {
             weight_t path_via_vertex = vertex_dist + weight;
             if (dist[neighbour] > path_via_vertex) {
-                dist[neighbour] = path_via_vertex;
+                dist[neighbour]      = path_via_vertex;
                 ancestors[neighbour] = vertex;
-                not_visited.Add(path_via_vertex, neighbour);
+                not_visited.push(path_via_vertex, neighbour);
             }
         }
 
@@ -235,35 +216,35 @@ static uint32_t log2_floor(size_t n) noexcept {
 }
 
 template <size_t C = 2>
-std::pair<vector<weight_t>, vector<vertex_t>> FindShortestPaths(const graph_t& g, uint32_t from) {
+std::pair<vector<weight_t>, vector<vertex_t>> shortest_paths(const graph_t& g,
+                                                             uint32_t from) {
     const size_t n = g.size();
-    size_t edges = 0;
+    size_t edges   = 0;
     for (const auto& neighbours : g) {
         edges += neighbours.size();
     }
     vector<weight_t> dist(n, kInfDist);
     vector<vertex_t> ancestors(n, kNoVertex);
     if (edges * log2_floor(n) <= n * n * C) {
-        FindShortestPathsLowDenisty(g, dist, ancestors, from);
+        find_shr_pths_lowdensity(g, dist, ancestors, from);
+    } else {
+        find_shr_pths_highdensity(g, dist, ancestors, from);
     }
-    else {
-        FindShortestPathsHighDenisty(g, dist, ancestors, from);
-    }
-    return { dist, ancestors };
+    return {dist, ancestors};
 }
 
-} // namespace dijkstra
+}  // namespace dijkstra
 
 int main() {
     using namespace dijkstra;
     graph_t g = {
-        { { 1, 1 }, { 2, 1 } },
-        { { 0, 4 }, { 2, 1 } },
-        { { 0, 2 }, { 1, 1 } },
+        {{1, 1}, {2, 1}},
+        {{0, 4}, {2, 1}},
+        {{0, 2}, {1, 1}},
     };
 
     {
-        auto [dist, ancestors] = FindShortestPaths(g, 0);
+        auto [dist, ancestors] = shortest_paths(g, 0);
         assert(dist[0] == 0);
         assert(dist[1] == 1);
         assert(dist[2] == 1);
@@ -272,7 +253,7 @@ int main() {
         assert(ancestors[2] == 0);
     }
     {
-        auto [dist, ancestors] = FindShortestPaths(g, 1);
+        auto [dist, ancestors] = shortest_paths(g, 1);
         assert(dist[0] == 3);
         assert(dist[1] == 0);
         assert(dist[2] == 1);
@@ -281,7 +262,7 @@ int main() {
         assert(ancestors[2] == 1);
     }
     {
-        auto [dist, ancestors] = FindShortestPaths(g, 2);
+        auto [dist, ancestors] = shortest_paths(g, 2);
         assert(dist[0] == 2);
         assert(dist[1] == 1);
         assert(dist[2] == 0);
