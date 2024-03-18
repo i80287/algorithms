df077a3a5ed1f6d33adde72117c27c3fa2c0d5e8
i80287 <addmlbx@gmail.com>
2024-03-18 23:06:31 +0300

    update DSU interface

--- DisjointSetUnionDS/main.cpp
+++ DisjointSetUnionDS/main.cpp
@@ -15,7 +15,7 @@ template <typename NodeType>
         { node->parent_ } -> std::convertible_to<NodeType*>;
     }
 #endif
-static NodeType* FindRoot(NodeType* node) noexcept {
+static NodeType* findRoot(NodeType* node) noexcept {
     NodeType* current_node = node;
     assert(current_node != nullptr);
     while (current_node->parent_ != nullptr) {
@@ -93,28 +93,28 @@ public:
     DisjointSetUnion& operator=(DisjointSetUnion&& other) noexcept {
         auto nodes = other.nodes_;
         auto nodes_count = other.nodes_count_;
-        this->~DisjointSetUnion();
         other.nodes_ = nullptr;
         other.nodes_count_ = 0;
+        this->~DisjointSetUnion();
         nodes_ = nodes;
         nodes_count_ = nodes_count;
         return *this;
     }
 
     // O(log*(n))
-    bool Equivalent(size_t node_x_index,
+    bool equal(size_t node_x_index,
                                      size_t node_y_index) noexcept {
         assert(node_x_index < nodes_count_ && node_y_index < nodes_count_);
-        return FindRoot(&nodes_[node_x_index]) ==
-               FindRoot(&nodes_[node_y_index]);
+        return findRoot(&nodes_[node_x_index]) ==
+               findRoot(&nodes_[node_y_index]);
     }
 
     // O(log*(n))
-    void Unite(size_t node_x_index,
+    void unite(size_t node_x_index,
                          size_t node_y_index) noexcept {
         assert(node_x_index < nodes_count_ && node_y_index < nodes_count_);
-        DSUTreeNode* node_x_root_ptr = FindRoot(&nodes_[node_x_index]);
-        DSUTreeNode* node_y_root_ptr = FindRoot(&nodes_[node_y_index]);
+        DSUTreeNode* node_x_root_ptr = findRoot(&nodes_[node_x_index]);
+        DSUTreeNode* node_y_root_ptr = findRoot(&nodes_[node_y_index]);
         if (node_x_root_ptr == node_y_root_ptr) {
             // Do not unite already united nodes so that for each root node:
             // root_node->parent_ == nullptr
@@ -135,6 +135,10 @@ public:
         }
     }
 
+    void resetData() {
+        std::memset(nodes_, 0, sizeof(DSUTreeNode) * nodes_count_);
+    }
+
     // O(n)
     ~DisjointSetUnion() {
         operator delete(nodes_);
@@ -142,17 +146,17 @@ public:
     }
 };
 
-// Weighted node with rank heuristic
-struct WeightedDSUTreeNode {
-    WeightedDSUTreeNode* parent_;
-    size_t rank_;
-    int64_t weight_;
-};
-
 /// @brief See also class DisjointSetUnion and
 /// https://youtu.be/MmemGjxsZTc?si=NHMBw-KJmxeXvkNA
 class WeightedDisjointSetUnion {
 public:
+    // Weighted node with rank heuristic
+    struct WeightedDSUTreeNode {
+        WeightedDSUTreeNode* parent_;
+        size_t rank_;
+        int64_t weight_;
+    };
+
     WeightedDSUTreeNode* nodes_ = nullptr;
     size_t nodes_count_ = 0;
 
@@ -225,27 +229,27 @@ public:
     WeightedDisjointSetUnion& operator=(WeightedDisjointSetUnion&& other) noexcept {
         auto nodes = other.nodes_;
         auto nodes_count = other.nodes_count_;
-        this->~WeightedDisjointSetUnion();
         other.nodes_ = nullptr;
         other.nodes_count_ = 0;
+        this->~WeightedDisjointSetUnion();
         nodes_ = nodes;
         nodes_count_ = nodes_count;
         return *this;
     }
 
     // O(log*(n))
-    bool Equivalent(size_t node_x_index,
+    bool equal(size_t node_x_index,
                                      size_t node_y_index) noexcept {
         assert(node_x_index < nodes_count_ && node_y_index < nodes_count_);
-        return FindRoot(&nodes_[node_x_index]) ==
-               FindRoot(&nodes_[node_y_index]);
+        return findRoot(&nodes_[node_x_index]) ==
+               findRoot(&nodes_[node_y_index]);
     }
 
     // O(log*(n))
-    void Unite(size_t node_x_index, size_t node_y_index) noexcept {
+    void unite(size_t node_x_index, size_t node_y_index) noexcept {
         assert(node_x_index < nodes_count_ && node_y_index < nodes_count_);
-        WeightedDSUTreeNode* node_x_root_ptr = FindRoot(&nodes_[node_x_index]);
-        WeightedDSUTreeNode* node_y_root_ptr = FindRoot(&nodes_[node_y_index]);
+        WeightedDSUTreeNode* node_x_root_ptr = findRoot(&nodes_[node_x_index]);
+        WeightedDSUTreeNode* node_y_root_ptr = findRoot(&nodes_[node_y_index]);
         if (node_x_root_ptr == node_y_root_ptr) {
             // Do not unite already united nodes so that for each root node:
             // root_node->parent_ == nullptr
@@ -270,21 +274,25 @@ public:
     }
 
     // O(log*(n))
-    int64_t GetWeightInSet(size_t node_index) noexcept {
+    int64_t getWeightInSet(size_t node_index) noexcept {
         assert(node_index < nodes_count_);
-        return FindRoot(&nodes_[node_index])->weight_;
+        return findRoot(&nodes_[node_index])->weight_;
     }
 
     // O(log*(n))
-    void AddWeightInSet(size_t node_index, int64_t delta) {
+    void addWeightInSet(size_t node_index, int64_t delta) {
         assert(node_index < nodes_count_);
-        FindRoot(&nodes_[node_index])->weight_ += delta;
+        findRoot(&nodes_[node_index])->weight_ += delta;
     }
 
     // O(log*(n))
-    void SetWeightInSet(size_t node_index, int64_t weight) {
+    void setWeightInSet(size_t node_index, int64_t weight) {
         assert(node_index < nodes_count_);
-        FindRoot(&nodes_[node_index])->weight_ = weight;
+        findRoot(&nodes_[node_index])->weight_ = weight;
+    }
+
+    void resetData() {
+        std::memset(nodes_, 0, sizeof(WeightedDSUTreeNode) * nodes_count_);
     }
 
     // O(n)
@@ -301,20 +309,20 @@ void ConsoleTest() {
     DisjointSetUnion tree(10);
     std::cout << std::boolalpha;
 
-    std::cout << "0 eq 1: " << tree.Equivalent(0, 1) << '\n';
-    tree.Unite(0, 1);
-    std::cout << "0 eq 1: " << tree.Equivalent(0, 1) << '\n';
+    std::cout << "0 eq 1: " << tree.equal(0, 1) << '\n';
+    tree.unite(0, 1);
+    std::cout << "0 eq 1: " << tree.equal(0, 1) << '\n';
 
-    tree.Unite(1, 2);
-    tree.Unite(2, 3);
-    tree.Unite(4, 3);
-    tree.Unite(4, 9);
-    std::cout << "1 eq 9: " << tree.Equivalent(1, 9) << '\n';
-    std::cout << "1 eq 8: " << tree.Equivalent(1, 8) << '\n';
+    tree.unite(1, 2);
+    tree.unite(2, 3);
+    tree.unite(4, 3);
+    tree.unite(4, 9);
+    std::cout << "1 eq 9: " << tree.equal(1, 9) << '\n';
+    std::cout << "1 eq 8: " << tree.equal(1, 8) << '\n';
 
-    tree.Unite(8, 9);
+    tree.unite(8, 9);
 
-    std::cout << "1 eq 8: " << tree.Equivalent(1, 8) << '\n';
+    std::cout << "1 eq 8: " << tree.equal(1, 8) << '\n';
 }
 
 void TestDSU() {
@@ -322,24 +330,24 @@ void TestDSU() {
     DisjointSetUnion tree(N);
 
     for (size_t i = 1; i < N; i++) {
-        assert(!tree.Equivalent(i - 1, i));
+        assert(!tree.equal(i - 1, i));
     }
 
     for (size_t i = 0; i < N; i++) {
-        assert(tree.Equivalent(i, i));
+        assert(tree.equal(i, i));
     }
 
-    tree.Unite(0, 1);
-    tree.Unite(2, 3);
-    tree.Unite(0, 3);
+    tree.unite(0, 1);
+    tree.unite(2, 3);
+    tree.unite(0, 3);
     for (size_t i = 0; i <= 3; i++) {
         for (size_t j = 0; j <= 3; j++) {
-            assert(tree.Equivalent(i, j));
+            assert(tree.equal(i, j));
         }
     }
 
     for (size_t i = 4; i < N; i++) {
-        assert(!tree.Equivalent(i - 1, i));
+        assert(!tree.equal(i - 1, i));
     }
 
     /*
@@ -350,44 +358,44 @@ void TestDSU() {
      * 34            38
      *
      */
-    tree.Unite(34, 35);
-    assert(tree.Equivalent(34, 35));
-    assert(!tree.Equivalent(35, 36));
-    assert(!tree.Equivalent(36, 37));
-    assert(!tree.Equivalent(37, 38));
-    assert(!tree.Equivalent(38, 39));
-    tree.Unite(36, 37);
-    assert(tree.Equivalent(34, 35));
-    assert(!tree.Equivalent(35, 36));
-    assert(tree.Equivalent(36, 37));
-    assert(!tree.Equivalent(37, 38));
-    assert(!tree.Equivalent(38, 39));
-    tree.Unite(38, 39);
-    assert(tree.Equivalent(34, 35));
-    assert(!tree.Equivalent(35, 36));
-    assert(tree.Equivalent(36, 37));
-    assert(!tree.Equivalent(37, 38));
-    assert(tree.Equivalent(38, 39));
-    tree.Unite(35, 37);
-    assert(tree.Equivalent(34, 35));
-    assert(tree.Equivalent(35, 36));
-    assert(tree.Equivalent(36, 37));
-    assert(!tree.Equivalent(37, 38));
-    assert(tree.Equivalent(38, 39));
-    tree.Unite(37, 38);
-    assert(tree.Equivalent(34, 35));
-    assert(tree.Equivalent(35, 36));
-    assert(tree.Equivalent(36, 37));
-    assert(tree.Equivalent(37, 38));
-    assert(tree.Equivalent(38, 39));
+    tree.unite(34, 35);
+    assert(tree.equal(34, 35));
+    assert(!tree.equal(35, 36));
+    assert(!tree.equal(36, 37));
+    assert(!tree.equal(37, 38));
+    assert(!tree.equal(38, 39));
+    tree.unite(36, 37);
+    assert(tree.equal(34, 35));
+    assert(!tree.equal(35, 36));
+    assert(tree.equal(36, 37));
+    assert(!tree.equal(37, 38));
+    assert(!tree.equal(38, 39));
+    tree.unite(38, 39);
+    assert(tree.equal(34, 35));
+    assert(!tree.equal(35, 36));
+    assert(tree.equal(36, 37));
+    assert(!tree.equal(37, 38));
+    assert(tree.equal(38, 39));
+    tree.unite(35, 37);
+    assert(tree.equal(34, 35));
+    assert(tree.equal(35, 36));
+    assert(tree.equal(36, 37));
+    assert(!tree.equal(37, 38));
+    assert(tree.equal(38, 39));
+    tree.unite(37, 38);
+    assert(tree.equal(34, 35));
+    assert(tree.equal(35, 36));
+    assert(tree.equal(36, 37));
+    assert(tree.equal(37, 38));
+    assert(tree.equal(38, 39));
 
     for (size_t i = 1; i < N; i++) {
-        tree.Unite(i - 1, i);
+        tree.unite(i - 1, i);
     }
 
     for (size_t i = 0; i < N; i++) {
         for (size_t j = 0; j < N; j++) {
-            assert(tree.Equivalent(i, j));
+            assert(tree.equal(i, j));
         }
     }
 
@@ -399,35 +407,35 @@ void TestWightedDSU() {
     WeightedDisjointSetUnion tree(N);
 
     for (size_t i = 1; i < N; i++) {
-        assert(!tree.Equivalent(i - 1, i));
+        assert(!tree.equal(i - 1, i));
     }
 
     for (size_t i = 0; i < N; i++) {
-        assert(tree.Equivalent(i, i));
+        assert(tree.equal(i, i));
     }
 
-    tree.Unite(0, 1);
-    tree.Unite(2, 3);
-    tree.Unite(0, 3);
+    tree.unite(0, 1);
+    tree.unite(2, 3);
+    tree.unite(0, 3);
     for (size_t i = 0; i <= 3; i++) {
         for (size_t j = 0; j <= 3; j++) {
-            assert(tree.Equivalent(i, j));
+            assert(tree.equal(i, j));
         }
     }
 
-    tree.AddWeightInSet(0, 10);
-    tree.AddWeightInSet(2, 10);
+    tree.addWeightInSet(0, 10);
+    tree.addWeightInSet(2, 10);
     for (size_t i = 0; i <= 3; i++) {
-        assert(tree.GetWeightInSet(i) == 20);
+        assert(tree.getWeightInSet(i) == 20);
     }
 
-    tree.SetWeightInSet(0, 10);
+    tree.setWeightInSet(0, 10);
     for (size_t i = 0; i <= 3; i++) {
-        assert(tree.GetWeightInSet(i) == 10);
+        assert(tree.getWeightInSet(i) == 10);
     }
 
     for (size_t i = 4; i < N; i++) {
-        assert(!tree.Equivalent(i - 1, i));
+        assert(!tree.equal(i - 1, i));
     }
 
     /*
@@ -438,104 +446,104 @@ void TestWightedDSU() {
      * 34            38
      *
      */
-    tree.Unite(34, 35);
-    tree.AddWeightInSet(34, 2);
-    assert(tree.Equivalent(34, 35));
-    assert(!tree.Equivalent(35, 36));
-    assert(!tree.Equivalent(36, 37));
-    assert(!tree.Equivalent(37, 38));
-    assert(!tree.Equivalent(38, 39));
-    assert(tree.GetWeightInSet(34) == 2);
-    assert(tree.GetWeightInSet(35) == 2);
-    assert(tree.GetWeightInSet(36) == 0);
-    assert(tree.GetWeightInSet(37) == 0);
-    assert(tree.GetWeightInSet(38) == 0);
-    assert(tree.GetWeightInSet(39) == 0);
-    tree.Unite(36, 37);
-    tree.AddWeightInSet(37, 3);
-    assert(tree.Equivalent(34, 35));
-    assert(!tree.Equivalent(35, 36));
-    assert(tree.Equivalent(36, 37));
-    assert(!tree.Equivalent(37, 38));
-    assert(!tree.Equivalent(38, 39));
-    assert(tree.GetWeightInSet(34) == 2);
-    assert(tree.GetWeightInSet(35) == 2);
-    assert(tree.GetWeightInSet(36) == 3);
-    assert(tree.GetWeightInSet(37) == 3);
-    assert(tree.GetWeightInSet(38) == 0);
-    assert(tree.GetWeightInSet(39) == 0);
-    tree.Unite(38, 39);
-    tree.AddWeightInSet(38, 4);
-    assert(tree.Equivalent(34, 35));
-    assert(!tree.Equivalent(35, 36));
-    assert(tree.Equivalent(36, 37));
-    assert(!tree.Equivalent(37, 38));
-    assert(tree.Equivalent(38, 39));
-    assert(tree.GetWeightInSet(34) == 2);
-    assert(tree.GetWeightInSet(35) == 2);
-    assert(tree.GetWeightInSet(36) == 3);
-    assert(tree.GetWeightInSet(37) == 3);
-    assert(tree.GetWeightInSet(38) == 4);
-    assert(tree.GetWeightInSet(39) == 4);
-    tree.Unite(35, 37);
-    assert(tree.Equivalent(34, 35));
-    assert(tree.Equivalent(35, 36));
-    assert(tree.Equivalent(36, 37));
-    assert(!tree.Equivalent(37, 38));
-    assert(tree.Equivalent(38, 39));
-    assert(tree.GetWeightInSet(34) == 5);
-    assert(tree.GetWeightInSet(35) == 5);
-    assert(tree.GetWeightInSet(36) == 5);
-    assert(tree.GetWeightInSet(37) == 5);
-    assert(tree.GetWeightInSet(38) == 4);
-    assert(tree.GetWeightInSet(39) == 4);
-    tree.Unite(37, 38);
-    assert(tree.Equivalent(34, 35));
-    assert(tree.Equivalent(35, 36));
-    assert(tree.Equivalent(36, 37));
-    assert(tree.Equivalent(37, 38));
-    assert(tree.Equivalent(38, 39));
-    assert(tree.GetWeightInSet(34) == 9);
-    assert(tree.GetWeightInSet(35) == 9);
-    assert(tree.GetWeightInSet(36) == 9);
-    assert(tree.GetWeightInSet(37) == 9);
-    assert(tree.GetWeightInSet(38) == 9);
-    assert(tree.GetWeightInSet(39) == 9);
+    tree.unite(34, 35);
+    tree.addWeightInSet(34, 2);
+    assert(tree.equal(34, 35));
+    assert(!tree.equal(35, 36));
+    assert(!tree.equal(36, 37));
+    assert(!tree.equal(37, 38));
+    assert(!tree.equal(38, 39));
+    assert(tree.getWeightInSet(34) == 2);
+    assert(tree.getWeightInSet(35) == 2);
+    assert(tree.getWeightInSet(36) == 0);
+    assert(tree.getWeightInSet(37) == 0);
+    assert(tree.getWeightInSet(38) == 0);
+    assert(tree.getWeightInSet(39) == 0);
+    tree.unite(36, 37);
+    tree.addWeightInSet(37, 3);
+    assert(tree.equal(34, 35));
+    assert(!tree.equal(35, 36));
+    assert(tree.equal(36, 37));
+    assert(!tree.equal(37, 38));
+    assert(!tree.equal(38, 39));
+    assert(tree.getWeightInSet(34) == 2);
+    assert(tree.getWeightInSet(35) == 2);
+    assert(tree.getWeightInSet(36) == 3);
+    assert(tree.getWeightInSet(37) == 3);
+    assert(tree.getWeightInSet(38) == 0);
+    assert(tree.getWeightInSet(39) == 0);
+    tree.unite(38, 39);
+    tree.addWeightInSet(38, 4);
+    assert(tree.equal(34, 35));
+    assert(!tree.equal(35, 36));
+    assert(tree.equal(36, 37));
+    assert(!tree.equal(37, 38));
+    assert(tree.equal(38, 39));
+    assert(tree.getWeightInSet(34) == 2);
+    assert(tree.getWeightInSet(35) == 2);
+    assert(tree.getWeightInSet(36) == 3);
+    assert(tree.getWeightInSet(37) == 3);
+    assert(tree.getWeightInSet(38) == 4);
+    assert(tree.getWeightInSet(39) == 4);
+    tree.unite(35, 37);
+    assert(tree.equal(34, 35));
+    assert(tree.equal(35, 36));
+    assert(tree.equal(36, 37));
+    assert(!tree.equal(37, 38));
+    assert(tree.equal(38, 39));
+    assert(tree.getWeightInSet(34) == 5);
+    assert(tree.getWeightInSet(35) == 5);
+    assert(tree.getWeightInSet(36) == 5);
+    assert(tree.getWeightInSet(37) == 5);
+    assert(tree.getWeightInSet(38) == 4);
+    assert(tree.getWeightInSet(39) == 4);
+    tree.unite(37, 38);
+    assert(tree.equal(34, 35));
+    assert(tree.equal(35, 36));
+    assert(tree.equal(36, 37));
+    assert(tree.equal(37, 38));
+    assert(tree.equal(38, 39));
+    assert(tree.getWeightInSet(34) == 9);
+    assert(tree.getWeightInSet(35) == 9);
+    assert(tree.getWeightInSet(36) == 9);
+    assert(tree.getWeightInSet(37) == 9);
+    assert(tree.getWeightInSet(38) == 9);
+    assert(tree.getWeightInSet(39) == 9);
     for (size_t i = 34; i <= 39; i++) {
         for (size_t j = 34; j <= 39; j++) {
-            assert(tree.Equivalent(i, j));
+            assert(tree.equal(i, j));
         }
     }
 
     for (size_t i = 1; i < N; i++) {
-        tree.Unite(i - 1, i);
+        tree.unite(i - 1, i);
     }
 
     for (size_t i = 0; i < N; i++) {
         for (size_t j = 0; j < N; j++) {
-            assert(tree.Equivalent(i, j));
+            assert(tree.equal(i, j));
         }
 
-        assert(tree.GetWeightInSet(i) == 10 + 9);
+        assert(tree.getWeightInSet(i) == 10 + 9);
     }
 
     std::vector<int64_t> vec{1, 2, 4, 8, 16, 32, 64};
     size_t n = vec.size();
     WeightedDisjointSetUnion wdsu{vec};
     for (size_t i = 0; i < n; i++) {
-        assert(wdsu.GetWeightInSet(i) == vec[i]);
+        assert(wdsu.getWeightInSet(i) == vec[i]);
     }
 
-    wdsu.Unite(0, 1);
-    wdsu.Unite(2, 3);
-    wdsu.Unite(0, 2);
+    wdsu.unite(0, 1);
+    wdsu.unite(2, 3);
+    wdsu.unite(0, 2);
     int64_t sum = vec[0] + vec[1] + vec[2] + vec[3];
     for (size_t i = 0; i <= 3; i++) {
-        assert(wdsu.GetWeightInSet(i) == sum);
+        assert(wdsu.getWeightInSet(i) == sum);
     }
 
     for (size_t i = 1; i < n; i++) {
-        wdsu.Unite(i - 1, i);
+        wdsu.unite(i - 1, i);
     }
 
     for (size_t i = 4; i < n; i++) {
@@ -543,7 +551,7 @@ void TestWightedDSU() {
     }
 
     for (size_t i = 0; i < n; i++) {
-        assert(wdsu.GetWeightInSet(i) == sum);
+        assert(wdsu.getWeightInSet(i) == sum);
     }
 
     puts("WeightedDisjointSetUnion tests passed");
