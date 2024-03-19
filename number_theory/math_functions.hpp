58e703f2d746e2255f03d71a3cb465689bf5ec45
i80287 <addmlbx@gmail.com>
2024-03-19 14:03:59 +0300

    added umod_128_64(uint128_t, uint64_t)

--- number_theory/math_functions.hpp
+++ number_theory/math_functions.hpp
@@ -1264,6 +1264,106 @@ ATTRIBUTE_CONST constexpr std::pair<T, uint32_t> extract_pow2(T n) noexcept {
     return {n >> r, r};
 }
 
+/// @brief Modified version of __udivmodti4 from the clang
+/// @param[in] a
+/// @param[in] b
+/// @return a % b
+ATTRIBUTE_CONST constexpr uint64_t umod_128_64(uint128_t a, uint64_t b) noexcept {
+    union utwords {
+        uint128_t all;
+        struct {
+#if CONFIG_BYTE_ORDER_LITTLE_ENDIAN
+            uint64_t low;
+            uint64_t high;
+#else
+            uint64_t high;
+            uint64_t low;
+#endif  // CONFIG_BYTE_ORDER_LITTLE_ENDIAN
+        } s;
+    };
+
+    const uint32_t n_udword_bits = sizeof(uint64_t) * CHAR_BIT;
+    const uint32_t n_utword_bits = sizeof(uint128_t) * CHAR_BIT;
+    utwords n;
+    n.all = a;
+    utwords d;
+    d.all = b;
+    utwords q;
+    utwords r;
+    // special cases, X is unknown, K != 0
+    if (n.s.high == 0) {
+        // 0 X
+        // ---
+        // 0 X
+        return n.s.low % b;
+    }
+    // n.s.high != 0
+
+    // K X
+    // ---
+    // 0 X
+    if (unlikely((b & (b - 1)) == 0)) /* if b is a power of 2 or 0 */ {
+        return n.s.low & (b - 1);
+    }
+
+    // K X
+    // ---
+    // 0 K
+    ATTRIBUTE_ASSUME(n.s.high != 0);
+    ATTRIBUTE_ASSUME(b != 0);
+    uint32_t sr = 1 + n_udword_bits + uint32_t(math_functions::countl_zero(b)) -
+                  uint32_t(math_functions::countl_zero(n.s.high));
+    // 2 <= sr <= n_utword_bits - 1
+    ATTRIBUTE_ASSUME(2 <= sr);
+    ATTRIBUTE_ASSUME(sr <= n_utword_bits - 1);
+    // q.all = n.all << (n_utword_bits - sr);
+    // r.all = n.all >> sr;
+    if (sr == n_udword_bits) {
+        q.s.low = 0;
+        q.s.high = n.s.low;
+        r.s.high = 0;
+        r.s.low = n.s.high;
+    } else if (sr < n_udword_bits) /* 2 <= sr <= n_udword_bits - 1 */ {
+        q.s.low = 0;
+        q.s.high = n.s.low << (n_udword_bits - sr);
+        r.s.high = n.s.high >> sr;
+        r.s.low = (n.s.high << (n_udword_bits - sr)) | (n.s.low >> sr);
+    } else /* n_udword_bits + 1 <= sr <= n_utword_bits - 1 */ {
+        q.s.low = n.s.low << (n_utword_bits - sr);
+        q.s.high = (n.s.high << (n_utword_bits - sr)) |
+                   (n.s.low >> (sr - n_udword_bits));
+        r.s.high = 0;
+        r.s.low = n.s.high >> (sr - n_udword_bits);
+    }
+
+    // Not a special case
+    // q and r are initialized with:
+    // q.all = n.all << (n_utword_bits - sr);
+    // r.all = n.all >> sr;
+    // 1 <= sr <= n_utword_bits - 1
+    uint32_t carry = 0;
+    for (; sr > 0; --sr) {
+        // r:q = ((r:q)  << 1) | carry
+        r.s.high = (r.s.high << 1) | (r.s.low >> (n_udword_bits - 1));
+        r.s.low = (r.s.low << 1) | (q.s.high >> (n_udword_bits - 1));
+        q.s.high = (q.s.high << 1) | (q.s.low >> (n_udword_bits - 1));
+        q.s.low = (q.s.low << 1) | carry;
+        // carry = 0;
+        // if (r.all >= d.all)
+        // {
+        //     r.all -= d.all;
+        //      carry = 1;
+        // }
+        const int128_t s = int128_t(d.all - r.all - 1) >> (n_utword_bits - 1);
+        carry = s & 1;
+        r.all -= d.all & uint128_t(s);
+    }
+
+    q.all = (q.all << 1) | carry;
+    ATTRIBUTE_ASSUME(r.s.high == 0);
+    return r.s.low;
+}
+
 namespace impl {
 
 /// @brief Returns max number of possible different
