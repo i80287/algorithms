26b05c8bf9a499ea713e5c96b3163749379c1f46
i80287 <addmlbx@gmail.com>
2024-06-03 22:38:17 +0300

    update tests

--- number_theory/run_tests.sh
+++ number_theory/run_tests.sh
@@ -8,10 +8,10 @@ cd ./build_tests || return 1
 # Why we export CC and CXX instead of overwriting CMAKE_C_COMPILER and CMAKE_CXX_COMPILER:
 #  https://stackoverflow.com/questions/17275348/how-to-specify-new-gcc-path-for-cmake
 
-# export CC=/usr/bin/gcc
-# export CXX=/usr/bin/g++
-# # GCC_RESULT="$(cmake -DCMAKE_BUILD_TYPE=Release -S .. -B . && make all --jobs 4 && make test)"
-# cmake -DCMAKE_BUILD_TYPE=Release -S .. -B . && make all --jobs 4 && make test
+export CC=/usr/bin/gcc
+export CXX=/usr/bin/g++
+# GCC_RESULT="$(cmake -DCMAKE_BUILD_TYPE=Release -S .. -B . && make all --jobs 4 && make test)"
+cmake -DCMAKE_BUILD_TYPE=Release -S .. -B . && make all --jobs 4 && make test
 
 export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
