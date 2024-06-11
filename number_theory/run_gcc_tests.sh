be1bd5bcc43dc9006d882e1d87391b8b1208edd7
i80287 <addmlbx@gmail.com>
2024-06-11 14:15:21 +0300

    updaate test running scripts

--- number_theory/run_gcc_tests.sh
+++ number_theory/run_gcc_tests.sh
@@ -5,7 +5,8 @@ cp ./u64-primes.txt ./build_tests/u64-primes.txt
 cp ./u128-primes.txt ./build_tests/u128-primes.txt
 cd ./build_tests || return 1
 
-export CC=/usr/bin/gcc
-export CXX=/usr/bin/g++
-
-cmake -D CMAKE_BUILD_TYPE=Release -S .. -B . && make all --jobs "$(nproc)" && make test
+export CC="/usr/bin/gcc"
+export CXX="/usr/bin/g++"
+cmake -D CMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER="$(CC)" -DCMAKE_CXX_COMPILER="$(CXX)" -S .. -B . \
+        && make all --jobs "$(nproc)" \
+        && make test
