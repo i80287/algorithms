3d38e86f1ecc08f011ca64d637f0179926f4671d
i80287 <addmlbx@gmail.com>
2024-06-13 17:42:29 +0300

    update build scripts

--- number_theory/run_clang_tests.sh
+++ number_theory/run_clang_tests.sh
@@ -5,8 +5,6 @@ cp ./u64-primes.txt ./build_tests/u64-primes.txt
 cp ./u128-primes.txt ./build_tests/u128-primes.txt
 cd ./build_tests || return 1
 
-export CC="/usr/bin/clang"
-export CXX="/usr/bin/clang++"
-cmake -D CMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER="$(CC)" -DCMAKE_CXX_COMPILER="$(CXX)" -S .. -B . \
+cmake -D CMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -S .. -B . \
         && make all --jobs "$(nproc)" \
         && make test
