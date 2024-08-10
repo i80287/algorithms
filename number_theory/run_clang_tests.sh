f8224441a1085c3ea0c723a89ea3f048d23f6e5a
i80287 <addmlbx@gmail.com>
2024-08-10 22:12:30 +0300

    update test running scripts

--- number_theory/run_clang_tests.sh
+++ number_theory/run_clang_tests.sh
@@ -7,4 +7,4 @@ cd ./build_tests || return 1
 
 cmake -D CMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -S .. -B . \
         && make all --jobs "$(nproc)" \
-        && make test
+        && env CTEST_OUTPUT_ON_FAILURE=1 make test
--- number_theory/run_gcc_tests.sh
+++ number_theory/run_gcc_tests.sh
@@ -7,4 +7,4 @@ cd ./build_tests || return 1
 
 cmake -D CMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -S .. -B . \
         && make all --jobs "$(nproc)" \
-        && make test
+        && env CTEST_OUTPUT_ON_FAILURE=1 make test
