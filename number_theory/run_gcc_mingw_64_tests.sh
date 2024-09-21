8df58976366b8a79ed4581321ebe2558f980f18d
i80287 <addmlbx@gmail.com>
2024-09-21 04:14:43 +0300

    update test running configs

--- number_theory/run_gcc_mingw_64_tests.sh
+++ number_theory/run_gcc_mingw_64_tests.sh
@@ -9,3 +9,8 @@ cd ./build_tests || return 1
 cmake -D CMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ -S .. -B . &&
     make clean &&
     make all --jobs "$(nproc)"
+
+shopt -s nullglob
+for test_executable in *.exe; do
+    wine "$test_executable" || exit 1
+done
