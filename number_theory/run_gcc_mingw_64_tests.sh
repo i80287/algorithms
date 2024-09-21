5022c9e7561e3f3aa9313cf1ed79927e2d0850b7
i80287 <addmlbx@gmail.com>
2024-09-21 03:47:42 +0300

    update testing scripts, add gcc-mingw-w64

--- number_theory/run_gcc_mingw_64_tests.sh
+++ number_theory/run_gcc_mingw_64_tests.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 
 if [ -d build_tests ]; then rm -r build_tests; fi
 mkdir -p build_tests
@@ -6,6 +6,6 @@ cp ./u64-primes.txt ./build_tests/u64-primes.txt
 cp ./u128-primes.txt ./build_tests/u128-primes.txt
 cd ./build_tests || return 1
 
-cmake -D CMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -S .. -B . &&
-        make all --jobs "$(nproc)" &&
-        env CTEST_OUTPUT_ON_FAILURE=1 make test
+cmake -D CMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ -S .. -B . &&
+    make clean &&
+    make all --jobs "$(nproc)"
--- number_theory/run_gcc_tests.sh
+++ number_theory/run_gcc_tests.sh
@@ -1,10 +1,11 @@
 #! /bin/sh
 
+if [ -d build_tests ]; then rm -r build_tests; fi
 mkdir -p build_tests
 cp ./u64-primes.txt ./build_tests/u64-primes.txt
 cp ./u128-primes.txt ./build_tests/u128-primes.txt
 cd ./build_tests || return 1
 
-cmake -D CMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -S .. -B . \
-        && make all --jobs "$(nproc)" \
-        && env CTEST_OUTPUT_ON_FAILURE=1 make test
+cmake -D CMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -S .. -B . &&
+        make all --jobs "$(nproc)" &&
+        env CTEST_OUTPUT_ON_FAILURE=1 make test
