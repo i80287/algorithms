3a986210349458ebf496139499f611f2dbb9320a
i80287 <addmlbx@gmail.com>
2024-09-23 23:46:06 +0300

    move CMakeLists.txt and test scripts

--- run_clang_tests.sh
+++ run_clang_tests.sh
@@ -6,8 +6,8 @@ set -e
 
 if [ -d "$build_dir" ]; then rm -r "$build_dir"; fi
 mkdir -p "$build_dir"
-cp ./u64-primes.txt ./$build_dir/u64-primes.txt
-cp ./u128-primes.txt ./$build_dir/u128-primes.txt
+cp ./number_theory/u64-primes.txt ./$build_dir/u64-primes.txt
+cp ./number_theory/u128-primes.txt ./$build_dir/u128-primes.txt
 cd ./$build_dir || return 1
 
 cmake -D CMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -S .. -B . &&
