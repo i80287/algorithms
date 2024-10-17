38ec2238c66676045969f5a52c22e4f497baada6
i80287 <addmlbx@gmail.com>
2024-10-17 23:43:21 +0300

    update tests and codechecker scripts

--- tests/run_codechecker.sh
+++ tests/run_codechecker.sh
@@ -4,6 +4,7 @@ build_dir=cmake-build-codechecker
 cmake_build_dir=cmake-build-dir
 
 mkdir -p "$build_dir"
+cp ./.clang-tidy ./$build_dir
 cd ./$build_dir || exit 1
 
 OLDIFS=$IFS
@@ -18,9 +19,14 @@ for cc_and_cxx in clang,clang++ gcc,g++ i686-w64-mingw32-gcc-posix,i686-w64-ming
         -DCMAKE_CXX_COMPILER="$2" \
         -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
         -S .. \
-        -B "$cmake_build_dir"
-    cd "./$cmake_build_dir" && make all --jobs "$(nproc)" && cd ..
-    CodeChecker analyze "./$cmake_build_dir/compile_commands.json" -o ./report_"$1"
+        -B $cmake_build_dir
+    cd ./$cmake_build_dir && make all --jobs "$(nproc)" && cd ..
+    CodeChecker analyze \
+        ./$cmake_build_dir/compile_commands.json \
+        --analyzer-config cppcheck:addons=../cppcheck/addons/misc.py \
+        --analyzer-config cppcheck:addons=../cppcheck/addons/y2038.py \
+        --analyzer-config clang-tidy:take-config-from-directory=true \
+        -o ./report_"$1"
     CodeChecker parse --print-steps ./report_"$1" >./parsed_report_"$1"
 done
 IFS=$OLDIFS
