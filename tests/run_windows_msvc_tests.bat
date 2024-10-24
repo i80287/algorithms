c51928b556f72acc64d9a87244746a9a63368e00
i80287 <addmlbx@gmail.com>
2024-10-25 00:02:09 +0300

    update windows tests running scripts

--- tests/run_windows_msvc_tests.bat
+++ tests/run_windows_msvc_tests.bat
@@ -13,7 +13,7 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 cmake -G "Visual Studio 17 2022" -A x64 -S .. -B .
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-msbuild tests.sln -property:Configuration=RelWithDebInfo -noWarn:D9025 -maxcpucount:%NUMBER_OF_PROCESSORS%
+msbuild tests.sln -property:Configuration=RelWithDebInfo -noWarn:D9025 -warnaserror -maxcpucount:%NUMBER_OF_PROCESSORS%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 msbuild RUN_TESTS.vcxproj -property:Configuration=RelWithDebInfo
