f721971bb0613244e193cd4894699db8a623a5ec
i80287 <addmlbx@gmail.com>
2024-10-24 23:00:46 +0300

    update test running scripts

---
+++ tests/prepare_stage_for_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/env bash
+
+function prepare_tests_data_and_cd_to_build_dir() {
+    build_dir="$1"
+    if [ -d "$build_dir" ]; then rm -r "$build_dir"; fi
+    mkdir -p "$build_dir"
+    cp ../number_theory/u64-primes.txt "./$build_dir/u64-primes.txt"
+    cp ../number_theory/u128-primes.txt "./$build_dir/u128-primes.txt"
+    cp ../tf_idf_actrie/Anglo_Saxon_Chronicle.txt "./$build_dir/Anglo_Saxon_Chronicle.txt"
+    cd "./$build_dir" || return 1
+}
