0f6ce8c2dc516ce9ba319fdce7c04487dd55cfaf
i80287 <addmlbx@gmail.com>
2024-12-20 03:38:33 +0300

    update test running scripts for windows

--- tests/run_windows_clang_cl_tests.bat
+++ tests/run_windows_clang_cl_tests.bat
@@ -5,15 +5,16 @@ if not exist ".\%build_dir%" mkdir %build_dir%
 copy "..\number_theory\u64-primes.txt" ".\%build_dir%\u64-primes.txt"
 copy "..\number_theory\u128-primes.txt" ".\%build_dir%\u128-primes.txt"
 copy "..\tf_idf_actrie\Anglo_Saxon_Chronicle.txt" ".\%build_dir%\Anglo_Saxon_Chronicle.txt"
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+cmake -G "Visual Studio 17 2022" -A x64 -T ClangCL -S . -B "%build_dir%"
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+@REM msbuild tests.sln -property:Configuration=RelWithDebInfo -noWarn:D9025 -warnaserror -maxcpucount:%NUMBER_OF_PROCESSORS%
+cmake --build "%build_dir%" --config RelWithDebInfo --parallel
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 cd ".\%build_dir%"
-
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-cmake -G "Visual Studio 17 2022" -A x64 -T ClangCL -S .. -B .
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-msbuild tests.sln -property:Configuration=RelWithDebInfo -noWarn:D9025 -warnaserror -maxcpucount:%NUMBER_OF_PROCESSORS%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 msbuild RUN_TESTS.vcxproj -property:Configuration=RelWithDebInfo
