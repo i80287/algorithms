4e1be0485a08a4c0b110563f22435381fb36e55d
i80287 <addmlbx@gmail.com>
2025-04-08 18:43:28 +0300

    update fft.hpp

--- number_theory/fft.hpp
+++ number_theory/fft.hpp
@@ -3,6 +3,12 @@
 #include <cassert>
 #include <cmath>
 #include <complex>
+#include <cstddef>
+#if defined(__cpp_exceptions) && __cpp_exceptions >= 199711
+#include <stdexcept>
+#else
+#include <exception>
+#endif
 #include <utility>
 #include <vector>
 
@@ -17,6 +23,8 @@ namespace fft {
 using f64 = double;
 using complex = std::complex<f64>;
 
+using std::size_t;
+
 /// @brief Multiply polinomials @a p1 and @a p2 of size @a n
 ///         and store their product into @a p2
 /// @param p1
@@ -28,7 +36,7 @@ ATTRIBUTE_NONNULL(1)
 ATTRIBUTE_NONNULL(2)
 inline void forward_backward_fft(complex* RESTRICT_QUALIFIER p1,
                                  complex* RESTRICT_QUALIFIER p2,
-                                 const std::size_t n);
+                                 size_t n);
 
 namespace detail {
 
@@ -41,12 +49,15 @@ private:
      * (fft_roots[0] is never used btw, because in fft step >= 1,
      *  so fft_roots[0] can be initialized with anything)
      */
-    static inline std::vector<complex> fft_roots = {complex(0, 0), complex(1, 0)};
+    static inline std::vector<complex> fft_roots = {
+        complex{0, 0},
+        complex{1, 0},
+    };
 
     template <bool IsBackwardFFT = false /* Forward of backward FFT */>
     ATTRIBUTE_SIZED_ACCESS(read_write, 1, 2)
     ATTRIBUTE_NONNULL(1) static void forward_or_backward_fft(complex* const p,
-                                                             const std::size_t k) noexcept {
+                                                             const size_t k) noexcept {
         CONFIG_ASSUME_STATEMENT(k > 0 && (k & (k - 1)) == 0);
 
         for (std::size_t i = 1, k_reversed_i = 0; i < k; i++) {
@@ -100,10 +111,40 @@ private:
         }
     }
 
-    static void ensure_roots_capacity(const std::size_t n) {
-        CONFIG_ASSUME_STATEMENT(n > 0 && (n & (n - 1)) == 0);
+    static void ensure_roots_capacity(const size_t n) {
+        ensure_roots_capacity_impl(n, fft_roots);
+    }
 
-        std::size_t current_len = fft_roots.size();
+    ATTRIBUTE_NORETURN
+    ATTRIBUTE_COLD
+    static void throw_impl(const char* const message) {
+#if defined(__cpp_exceptions) && __cpp_exceptions >= 199711
+        throw std::runtime_error{message};
+#else
+        std::terminate();
+#endif
+    }
+
+    ATTRIBUTE_NONNULL(2)
+    static void throw_if_not_impl(const bool expr, const char* const message) {
+        if (unlikely(!expr)) {
+            throw_impl(message);
+        }
+    }
+
+#define THROW_IF_NOT2(expr, expr_str, file_name_str, line_int) \
+    throw_if_not_impl(                                         \
+        expr, "Expression \"" expr_str "\" evaluated to false at " file_name_str ":" #line_int)
+
+#define THROW_IF_NOT1(expr, expr_str, file_name_str, line_int) \
+    THROW_IF_NOT2(expr, expr_str, file_name_str, line_int)
+
+#define THROW_IF_NOT(expr) THROW_IF_NOT1(expr, #expr, __FILE__, __LINE__)
+
+    static void ensure_roots_capacity_impl(const size_t n, std::vector<complex>& roots) {
+        THROW_IF_NOT(n > 0 && (n & (n - 1)) == 0);
+
+        size_t current_len = roots.size();
         assert(current_len >= 2 && (current_len & (current_len - 1)) == 0);
         CONFIG_ASSUME_STATEMENT(current_len >= 2 && (current_len & (current_len - 1)) == 0);
         if (current_len >= n) {
@@ -113,69 +154,73 @@ private:
         assert(n >= 4);
         CONFIG_ASSUME_STATEMENT(n >= 4);
 
-        fft_roots.reserve(n);
-        auto add_point = [fft_roots_data = fft_roots.data(), &current_len](std::size_t i) noexcept {
+        roots.reserve(n);
+        const auto add_point = [roots_data = roots.data(), &current_len,
+                                &roots](const size_t i) noexcept {
 #if defined(__cpp_lib_math_constants) && __cpp_lib_math_constants >= 201907L
             constexpr f64 kPi = std::numbers::pi_v<f64>;
 #else
             const f64 kPi = std::acos(static_cast<f64>(-1));
 #endif
-            fft_roots.emplace_back(fft_roots_data[i]);
+            roots.emplace_back(roots_data[i]);
             // double phi = 2 * kPi * (2 * i - current_len + 1) / (2 * current_len);
             const f64 phi =
                 kPi * static_cast<f64>(2 * i - current_len + 1) / static_cast<f64>(current_len);
-            fft_roots.emplace_back(std::cos(phi), std::sin(phi));
+            roots.emplace_back(std::cos(phi), std::sin(phi));
         };
         do {
-            for (std::size_t i = current_len / 2; i != current_len; i++) {
+            for (size_t i = current_len / 2; i != current_len; i++) {
                 add_point(i);
             }
             current_len *= 2;
         } while (current_len < n);
-        assert(fft_roots.size() == current_len);
+        assert(roots.size() == current_len);
         assert(current_len == n);
     }
 
-    friend inline void fft::forward_backward_fft(complex* p1, complex* p2, const std::size_t n);
+#undef THROW_IF_NOT
+#undef THROW_IF_NOT1
+#undef THROW_IF_NOT2
+
+    friend inline void fft::forward_backward_fft(complex* p1, complex* p2, size_t n);
 };
 
 }  // namespace detail
 
-inline void forward_backward_fft(complex* p1, complex* p2, const std::size_t n) {
+inline void forward_backward_fft(complex* const p1, complex* const p2, const size_t n) {
     assert(n > 0 && (n & (n - 1)) == 0);
     CONFIG_ASSUME_STATEMENT(n > 0 && (n & (n - 1)) == 0);
 
     fft::detail::private_impl::ensure_roots_capacity(n);
     fft::detail::private_impl::forward_or_backward_fft</*IsBackwardFFT = */ false>(p1, n);
+
+    // clang-format off
     /*
-     * A(w^j) = a_0 + a_1 * w^j + a_2 * w^{2 j} + ... + a_{n - 1} * w^{(n - 1)
-     * j} B(w^j) = b_0 + b_1 * w^j + b_2 * w^{2 j} + ... + b_{n - 1} * w^{(n -
-     * 1) j}
+     * A(w^j) = a_0 + a_1 * w^j + a_2 * w^{2 j} + ... + a_{n - 1} * w^{(n - 1) j}
+     * B(w^j) = b_0 + b_1 * w^j + b_2 * w^{2 j} + ... + b_{n - 1} * w^{(n - 1) j}
      *
-     * P = A + B * i = [ A(w^0) + B(w^0) * i, A(w^1) + B(w^1) * i, ... A(w^(n -
-     * 1)) + B(w^(n - 1)) * i ]
+     * P = A + B * i = [ A(w^0) + B(w^0) * i, A(w^1) + B(w^1) * i, ... A(w^(n - 1)) + B(w^(n - 1)) * i ]
      *
      * P(w^j) + conj(P(w^{n - j})) =
      * = A(w^j) + B(w^j) * i + conj(A(w^{n - j}) + B(w^{n - j}) * i) =
-     * = \sum{k=0}{n-1} (a_k + b_k * i) * w^{jk} + \sum{k=0}{n-1} conj((a_k +
-     * b_k * i) * w^{(n - j)k}) = = \sum{k=0}{n-1} (a_k + b_k * i) * w^{jk} +
-     * \sum{k=0}{n-1} conj((a_k + b_k * i) * w^{-jk}) = = \sum{k=0}{n-1} (a_k +
-     * b_k * i) * w^{jk} + conj((a_k + b_k * i) * w^{-jk}) = = \sum{k=0}{n-1}
-     * (a_k + b_k * i) * w^{jk} + conj(a_k + b_k * i) * conj(w^{-jk}) = =
-     * \sum{k=0}{n-1} (a_k + b_k * i) * w^{jk} + (a_k - b_k * i) * w^{jk} = =
-     * \sum{k=0}{n-1} 2 a_k * w^{jk} = 2 * A(w^j)
+     * = \sum_{k=0}^{n-1} (a_k + b_k * i) * w^{j k} + \sum_{k=0}^{n-1} conj((a_k + b_k * i) * w^{(n - j) k}) =
+     * = \sum_{k=0}^{n-1} (a_k + b_k * i) * w^{j k} + \sum_{k=0}^{n-1} conj((a_k + b_k * i) * w^{-j k}) =
+     * = \sum_{k=0}^{n-1} (a_k + b_k * i) * w^{j k} + conj((a_k + b_k * i) * w^{-j k}) =
+     * = \sum_{k=0}^{n-1} (a_k + b_k * i) * w^{j k} + conj(a_k + b_k * i) * conj(w^{-j k}) =
+     * = \sum_{k=0}^{n-1} (a_k + b_k * i) * w^{j k} + (a_k - b_k * i) * w^{j k} =
+     * = \sum_{k=0}^{n-1} 2 a_k * w^{j k} = 2 * A(w^j)
      *
      * \implies A(w^j) = (P(w^j) + conj(P(w^{n - j}))) / 2
      *
-     * By analogy it is can be proved that A(w^j) = (P(w^j) - conj(P(w^{n -
-     * j}))) / (2 * i)
+     * By analogy it is can be proved that
+     * B(w^j) = (P(w^j) - conj(P(w^{n - j}))) / (2 * i)
      *
      * C(w^j) = A(w^j) * B(w^j) \implies C(w^j) =
-     * = (P(w^j) + conj(P(w^{n - j}))) / 2 * (P(w^j) - conj(P(w^{n - j}))) / (2
-     * * i) = = (P(w^j) + conj(P(w^{n - j}))) * (P(w^j) - conj(P(w^{n - j}))) /
-     * (4 * i) = = (P(w^j) + conj(P(w^{n - j}))) * (P(w^j) - conj(P(w^{n - j})))
-     * / (4 * i) =
+     * = (P(w^j) + conj(P(w^{n - j}))) / 2 * (P(w^j) - conj(P(w^{n - j}))) / (2 * i) =
+     * = (P(w^j) + conj(P(w^{n - j}))) * (P(w^j) - conj(P(w^{n - j}))) / (4 * i)
      */
+    // clang-format on
+
     constexpr complex one_over_four_i(f64{0}, f64{-0.25});  // 1 / (4 * i) == -i / 4
     for (std::size_t j = 0; j < n; j++) {
         const std::size_t n_j = (n - j) & (n - 1);  // <=> mod n because n is power of two
