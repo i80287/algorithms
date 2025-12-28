c7db9d5c94f340f76b05dcffd469310ba83e19f5
i80287 <92759021+i80287@users.noreply.github.com>
2025-12-28 23:04:57 +0300

    [bugfix/fix_fedora_pipelines] make test fail on build error (#71)
    * [bugfix/fix_fedora_pipelines] make test fail on build error
    
    * [bugfix/fix_fedora_pipelines] update test scripts

--- tests/run_clang_tests.sh
+++ tests/run_clang_tests.sh
@@ -13,9 +13,8 @@ for force_disable_checks in ON OFF; do
             -D CMAKE_C_COMPILER=clang \
             -D CMAKE_CXX_COMPILER=clang++ \
             -D UTILS_FLAGS_FORCE_DISABLE_RUNTIME_CHECKS=$force_disable_checks \
-            -S .. -B . &&
-            cmake --build . --parallel &&
-            env CTEST_OUTPUT_ON_FAILURE=1 ctest
-
+            -S .. -B .
+    cmake --build . --parallel
+    env CTEST_OUTPUT_ON_FAILURE=1 ctest
     leave_test
 done
--- tests/run_gcc_tests.sh
+++ tests/run_gcc_tests.sh
@@ -4,18 +4,19 @@ set -e
 
 for force_disable_checks in ON OFF; do
     build_dir=cmake-build-tests-gcc
-    
+
     . ./prepare_stage_for_tests.sh
     prepare_tests_data_and_cd_to_build_dir "$build_dir"
-    
+
     cmake -G Ninja \
-            -D CMAKE_BUILD_TYPE=RelWithDebInfo \
-            -D CMAKE_C_COMPILER=gcc \
-            -D CMAKE_CXX_COMPILER=g++ \
-            -D UTILS_FLAGS_FORCE_DISABLE_RUNTIME_CHECKS=$force_disable_checks \
-            -S .. -B . &&
-            cmake --build . --parallel &&
-            env CTEST_OUTPUT_ON_FAILURE=1 ctest
+        -D CMAKE_BUILD_TYPE=RelWithDebInfo \
+        -D CMAKE_C_COMPILER=gcc \
+        -D CMAKE_CXX_COMPILER=g++ \
+        -D UTILS_FLAGS_FORCE_DISABLE_RUNTIME_CHECKS=$force_disable_checks \
+        -S .. \
+        -B .
+    cmake --build . --parallel
+    env CTEST_OUTPUT_ON_FAILURE=1 ctest
 
     leave_test
 done
